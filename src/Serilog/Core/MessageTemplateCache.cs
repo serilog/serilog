6b3ea5e4de4f05bb7c60f12c395822f679aabc78
nblumhardt <nblumhardt@nblumhardt.com>
2013-03-30 14:26:50 +1000

    Closed issue #12, safety backstop when excess message templates are generated.

--- src/Serilog/Core/MessageTemplateCache.cs
+++ src/Serilog/Core/MessageTemplateCache.cs
@@ -21,10 +21,23 @@ namespace Serilog.Core
     class MessageTemplateCache : IMessageTemplateCache
     {
         readonly ConcurrentDictionary<string, MessageTemplate> _templates = new ConcurrentDictionary<string,MessageTemplate>();
+        const int MaxCacheItems = 1000;
 
         public MessageTemplate GetParsedTemplate(string messageTemplate)
         {
             if (messageTemplate == null) throw new ArgumentNullException("messageTemplate");
+            
+            // This is *not* the sunny day scenario; all we're doing here is preventing out-of-memory
+            // conditions when the library is used incorrectly. Correct use (templates, rather than
+            // direct message strings) should barely ever overflow this cache.
+            if (_templates.Count > MaxCacheItems)
+            {
+                MessageTemplate result;
+                if (!_templates.TryGetValue(messageTemplate, out result))
+                    result = Parse(messageTemplate);
+                return result;
+            }
+
             return _templates.GetOrAdd(messageTemplate, Parse);
         }
 
