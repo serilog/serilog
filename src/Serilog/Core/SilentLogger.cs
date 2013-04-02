5140fb74462f98455eecef0c375208224fb690be
Glenn Morton <glenn@glennandkristy.com>
2013-04-02 22:57:32 +0800

    Moved ILogger methods to extension methods.

--- src/Serilog/Core/SilentLogger.cs
+++ src/Serilog/Core/SilentLogger.cs
@@ -51,53 +51,5 @@ namespace Serilog.Core
         {
             return false;
         }
-
-        public void Verbose(string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Verbose(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Debug(string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Debug(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Information(string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Information(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Warning(string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Warning(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Error(string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Error(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Fatal(string messageTemplate, params object[] propertyValues)
-        {
-        }
-
-        public void Fatal(Exception exception, string messageTemplate, params object[] propertyValues)
-        {
-        }
     }
 }
