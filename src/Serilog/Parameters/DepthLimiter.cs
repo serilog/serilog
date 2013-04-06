952288a4d45e054a2b8dfe2a246d3ae76f2541c0
nblumhardt <nblumhardt@nblumhardt.com>
2013-04-06 13:02:31 +1000

    Message when maximum destructuring depth reached.

--- src/Serilog/Parameters/DepthLimiter.cs
+++ src/Serilog/Parameters/DepthLimiter.cs
@@ -1,4 +1,19 @@
-﻿using Serilog.Core;
+﻿// Copyright 2013 Nicholas Blumhardt
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Serilog.Core;
+using Serilog.Debugging;
 using Serilog.Events;
 
 namespace Serilog.Parameters
@@ -19,7 +34,10 @@ namespace Serilog.Parameters
         public LogEventPropertyValue CreatePropertyValue(object value, bool destructureObjects = false)
         {
             if (_currentDepth == MaximumDestructuringDepth)
+            {
+                SelfLog.WriteLine("Maximium destructuring depth reached.");
                 return new ScalarValue(null);
+            }
 
             return _propertyValueConverter.CreatePropertyValue(value, destructureObjects, _currentDepth + 1);
         }
