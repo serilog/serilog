6503e6e96e374d9c2822c82552c915c22f5eea6b
nblumhardt <nblumhardt@nblumhardt.com>
2013-05-30 07:41:48 +1000

    A first cut at some 'friendly' filtering.

---
+++ src/Serilog/Filters/Matching.cs
@@ -0,0 +1,92 @@
+﻿// Copyright 2013 Nicholas Blumhardt
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Filters
+{
+    /// <summary>
+    /// Predicates applied to log events that can be used 
+    /// </summary>
+    public static class Matching
+    {
+        /// <summary>
+        /// Matches events from the specified source type.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <returns>A predicate for matching events.</returns>
+        public static Func<LogEvent, bool> FromSource<TSource>()
+        {
+            return WithProperty(Logger.SourceContextPropertyName, typeof(TSource).FullName);
+        }
+
+        /// <summary>
+        /// Matches events with the specified property attached,
+        /// regardless of its value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to match.</param>
+        /// <returns>A predicate for matching events.</returns>
+        public static Func<LogEvent, bool> WithProperty(string propertyName)
+        {
+            if (propertyName == null) throw new ArgumentNullException("propertyName");
+            return e => e.Properties.ContainsKey(propertyName);
+        }
+
+        /// <summary>
+        /// Matches events with the specified property value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to match.</param>
+        /// <param name="scalarValue">The property value to match; must be a scalar type.
+        /// Null is allowed.</param>
+        /// <returns>A predicate for matching events.</returns>
+        public static Func<LogEvent, bool> WithProperty(string propertyName, object scalarValue)
+        {
+            if (propertyName == null) throw new ArgumentNullException("propertyName");
+            var scalar = new ScalarValue(scalarValue);
+            return e =>
+            {
+                LogEventProperty property;
+                return e.Properties.TryGetValue(propertyName, out property) &&
+                    scalar.Equals(property.Value);
+            };
+        }
+
+        /// <summary>
+        /// Matches events with the specified property value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to match.</param>
+        /// <param name="predicate">A predicate for testing </param>
+        /// <typeparam name="TScalar">The type of scalar values to match.</typeparam>
+        /// <returns>A predicate for matching events.</returns>
+        public static Func<LogEvent, bool> WithProperty<TScalar>(string propertyName, Func<TScalar, bool> predicate)
+        {
+            if (propertyName == null) throw new ArgumentNullException("propertyName");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            return e =>
+            {
+                LogEventProperty property;
+                if (!e.Properties.TryGetValue(propertyName, out property)) return false;
+
+                var s = property.Value as ScalarValue;
+                if (s == null) return false;
+
+                return (s.Value is TScalar) &&
+                       predicate((TScalar)s.Value);
+            };
+        }
+    }   
+}
---
+++ test/Serilog.Tests/Filters/MatchingTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Serilog.Filters;
+using Serilog.Tests.Support;
+
+namespace Serilog.Tests.Filters
+{
+    [TestFixture]
+    public class MatchingTests
+    {
+        [Test]
+        public void EventsCanBeExcludedBySource()
+        {
+            var written = false;
+
+            var log = new LoggerConfiguration()
+                .Filter.ByExcluding(Matching.FromSource<MatchingTests>())
+                .WriteTo.Sink(new DelegatingSink(e => written = true))
+                .CreateLogger();
+
+            var sourceContext = log.ForContext<MatchingTests>();
+            sourceContext.Write(Some.InformationEvent());
+
+            Assert.IsFalse(written);
+        }
+
+        [Test]
+        public void EventsCanBeExcludedByPredicate()
+        {
+            var seen = 0;
+
+            var log = new LoggerConfiguration()
+                .Filter.ByExcluding(Matching.WithProperty<int>("Count", p => p < 10))
+                .WriteTo.Sink(new DelegatingSink(e => seen++))
+                .CreateLogger();
+
+            log.Warning("Unrelated");
+            log.Information("{Count}", 5);
+            log.Information("{Count}", "wrong type");
+            log.Information("{Count}", 15);
+
+            Assert.AreEqual(3, seen);
+        }
+    }
+}
