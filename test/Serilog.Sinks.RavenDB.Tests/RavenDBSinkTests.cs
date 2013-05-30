0d6f14683e210c00e8dfade28daa18172529a0bb
nblumhardt <nblumhardt@nblumhardt.com>
2013-05-30 21:12:14 +1000

    Getting to know the contributed RavenDB sink by tinkering with the test case.

--- test/Serilog.Sinks.RavenDB.Tests/RavenDBSinkTests.cs
+++ test/Serilog.Sinks.RavenDB.Tests/RavenDBSinkTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Runtime.Remoting.Channels;
 using NUnit.Framework;
 using Raven.Client.Embedded;
 using Serilog.Events;
@@ -15,24 +15,34 @@ namespace Serilog.Sinks.RavenDB.Tests
         static readonly TimeSpan TinyWait = TimeSpan.FromMilliseconds(50);
         
         [Test]
-        public void WhenAnEventIsEnqueuedItIsWrittenToABatch_OnTimer()
+        public void WhenAnEventIsWrittenToTheSinkItIsRetrievableFromTheDocumentStore()
         {
             using (var documentStore = new EmbeddableDocumentStore {RunInMemory = true}.Initialize())
             {
-                var timestamp = new DateTimeOffset(2013, 05, 28, 22, 10, 20, 666, new TimeSpan(0));
-                var ravenSink = new RavenDBSink(documentStore, 2, TinyWait, null);
-                var logEvent = new LogEvent(timestamp, LogEventLevel.Information, new ArgumentNullException("Mládek"), new MessageTemplateParser().Parse("Geneva"), new List<LogEventProperty> { new LogEventProperty("Youngblood", new ScalarValue("New Macabre")) });
-                ravenSink.Emit(logEvent);
-                Thread.Sleep(TinyWait + TinyWait);
-                ravenSink.Dispose();
+                var timestamp = new DateTimeOffset(2013, 05, 28, 22, 10, 20, 666, TimeSpan.FromHours(10));
+                var exception = new ArgumentException("Mládek");
+                const LogEventLevel level = LogEventLevel.Information;
+                const string messageTemplate = "{Song}";
+                var properties = new List<LogEventProperty> { new LogEventProperty("Song", new ScalarValue("New Macabre")) };
+
+                using (var ravenSink = new RavenDBSink(documentStore, 2, TinyWait, null))
+                {
+                    var template = new MessageTemplateParser().Parse(messageTemplate);
+                    var logEvent = new LogEvent(timestamp, level, exception, template, properties);
+                    ravenSink.Emit(logEvent);
+                }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var events = session.Query<LogEventEntity>().Customize(x => x.WaitForNonStaleResults()).ToList();
                     Assert.AreEqual(1, events.Count);
-                    Assert.AreEqual(timestamp, events.First().Timestamp);
-                    Assert.AreEqual(LogEventLevel.Information, events.First().Level);
-                    Assert.AreEqual(1, events.First().Properties.Count);
+                    var single = events.Single();
+                    Assert.AreEqual(messageTemplate, single.MessageTemplate);
+                    Assert.AreEqual("\"New Macabre\"", single.RenderedMessage);
+                    Assert.AreEqual(timestamp, single.Timestamp);
+                    Assert.AreEqual(level, single.Level);
+                    Assert.AreEqual(1, single.Properties.Count);
+                    Assert.AreEqual(exception.Message, single.Exception.Message);
                 }
             }
         }
