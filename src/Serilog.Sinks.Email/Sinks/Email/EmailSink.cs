c8fb5451d4bea51c07e79fbb422926c1650401ed
andymac4182 <andrew.mcclenaghan@gmail.com>
2013-10-18 11:51:56 +1000

    Adding email sink

---
+++ src/Serilog.Sinks.Email/Sinks/Email/EmailSink.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.PeriodicBatching;
+
+namespace Serilog.Sinks.Email
+{
+    class EmailSink : PeriodicBatchingSink
+    {
+        private readonly string _fromEmail;
+        private readonly string _toEmail;
+
+        private readonly SmtpClient _smtpClient;
+
+        readonly ITextFormatter _textFormatter;
+
+        /// <summary>
+        /// A reasonable default for the number of events posted in
+        /// each batch.
+        /// </summary>
+        public const int DefaultBatchPostingLimit = 100;
+
+
+        /// <summary>
+        /// A reasonable default time to wait between checking for event batches.
+        /// </summary>
+        public static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(30);
+
+        /// <summary>
+        /// Construct a sink emailing with the specified details.
+        /// </summary>
+        /// <param name="fromEmail">The email address emails will be sent from</param>
+        /// <param name="toEmail">The email address emails will be sent to</param>
+        /// <param name="mailServer">The SMTP email server to use</param>
+        /// <param name="networkCredential">The network credentials to use to authenticate with mailServer</param>
+        /// <param name="batchSizeLimit">The maximum number of events to post in a single batch.</param>
+        /// <param name="period">The time to wait between checking for event batches.</param>
+        /// <param name="textFormatter">Supplies culture-specific formatting information, or null.</param>
+        public EmailSink(string fromEmail, string toEmail, string mailServer, ICredentialsByHost networkCredential, int batchSizeLimit, TimeSpan period, ITextFormatter textFormatter) 
+            : base(batchSizeLimit, period)
+        {
+            if (fromEmail == null) throw new ArgumentNullException("fromEmail");
+            if (toEmail == null) throw new ArgumentNullException("toEmail");
+            if (mailServer == null) throw new ArgumentNullException("mailServer");
+
+            _fromEmail = fromEmail;
+            _toEmail = toEmail;
+            _textFormatter = textFormatter;
+
+            _smtpClient = new SmtpClient(mailServer) {Credentials = networkCredential};
+            _smtpClient.SendCompleted += SendCompletedCallback;
+        }
+
+        /// <summary>
+        /// Reports if there is an error in sending an email
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void SendCompletedCallback(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                SelfLog.WriteLine("Received failed result {0}: {1}", "Cancelled", e.Error);
+            }
+            if (e.Error != null)
+            {
+                SelfLog.WriteLine("Received failed result {0}: {1}", "Error", e.Error);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Free resources held by the sink.
+        /// </summary>
+        /// <param name="disposing">If true, called because the object is being disposed; if false,
+        /// the object is being disposed from the finalizer.</param>
+        protected override void Dispose(bool disposing)
+        {
+            // First flush the buffer
+            base.Dispose(disposing);
+
+            if (disposing)
+                _smtpClient.Dispose();
+        }
+
+
+        /// <summary>
+        /// Emit a batch of log events, running asynchronously.
+        /// </summary>
+        /// <param name="events">The events to emit.</param>
+        /// <remarks>Override either <see cref="PeriodicBatchingSink.EmitBatch"/> or <see cref="PeriodicBatchingSink.EmitBatchAsync"/>,
+        /// not both.</remarks>
+        protected override async Task EmitBatchAsync(IEnumerable<LogEvent> events)
+        {
+
+            if (events == null) throw new ArgumentNullException("events");
+            var payload = new StringWriter();
+
+            foreach (var logEvent in events)
+            {
+                _textFormatter.Format(logEvent, payload);
+            }
+
+            var mailMessage = new MailMessage(_fromEmail, _toEmail)
+            {
+                Subject = "Log Email",
+                Body = payload.ToString(),
+                BodyEncoding = Encoding.UTF8,
+                SubjectEncoding = Encoding.UTF8
+            };
+
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
