dcf6d96b9835d7686c0987880eb56d11fc5ed0eb
Scott Meyer <scott.meyer@firehost.com>
2013-11-11 16:56:07 -0600

    remove packages directory and enable nuget restore

--- packages/Glimpse.1.6.0/tools/init.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package)
-
-Import-Module (Join-Path $toolsPath glimpse.psm1)
--- packages/Glimpse.AspNet.1.3.1/tools/uninstall.ps1
+++
@@ -1,3 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-Unregister-GlimpseExtension $package
