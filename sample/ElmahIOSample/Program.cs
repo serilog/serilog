77c91a8f72390500a914a55cc3f79e878d2f601f
Michiel van Oudheusden <m.vanoudheusden@gmail.com>
2014-03-10 23:41:14 +0100

    Fine tune

--- sample/ElmahIOSample/Program.cs
+++ sample/ElmahIOSample/Program.cs
@@ -9,30 +9,19 @@ using Serilog;
 
 namespace ElmahIOSample
 {
-    class Position
-    {
-        public double Lat { get; set; }
-        public double Long { get; set; }
-    }
+
 
     class Program
     {
         static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-           .MinimumLevel.Verbose()
-           .WriteTo.ColoredConsole()
-           .WriteTo.ElmahIO(new Guid("c0576e7d-a98c-4c5f-b41c-9cd801df6f5d"))
-           .CreateLogger();
+             .MinimumLevel.Verbose()
+             .WriteTo.ColoredConsole()
+             .WriteTo.ElmahIO(new Guid(""))  // Enter your own elmah.io log id.
+             .CreateLogger();
 
             Log.Verbose("This app, {ExeName}, demonstrates the basics of using Serilog", "Demo.exe");
-
-            ProcessInput(new Position { Lat = 24.7, Long = 132.2 });
-            ProcessInput(new Position { Lat = 24.71, Long = 132.15 });
-            ProcessInput(new Position { Lat = 24.72, Long = 132.2 });
-       
-            const int failureCount = 3;
-            Log.Warning("Exception coming up because of {FailureCount} failures...", failureCount);
 
             try
             {
@@ -40,31 +29,29 @@ namespace ElmahIOSample
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "There's those {FailureCount} failures", failureCount);
+                Log.Error(ex, "We did some bad work here.");
+            }
+
+            var result = 0;
+            var divideBy = 0;
+            try
+            {
+                result = 10 / divideBy;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Unable to divide by {divideBy}", divideBy);
             }
 
             Log.Fatal("That's all folks - and all done using {WorkingSet} bytes of RAM", Environment.WorkingSet);
             Console.ReadKey(true);
         }
 
-
-
         static void DoBad()
         {
             throw new InvalidOperationException("Everything's broken!");
         }
 
-        static readonly Random Rng = new Random();
 
-        static void ProcessInput(Position sensorInput)
-        {
-            var sw = new Stopwatch();
-            sw.Start();
-            Log.Debug("Processing some input on {MachineName}...", Environment.MachineName);
-            Thread.Sleep(Rng.Next(0, 100));
-            sw.Stop();
-
-            Log.Information("Processed {@SensorInput} in {Time:000} ms", sensorInput, sw.ElapsedMilliseconds);
-        }
     }
 }
--- src/Serilog.Sinks.ElmahIO/LoggerConfigurationElmahIOExtensions.cs
+++ src/Serilog.Sinks.ElmahIO/LoggerConfigurationElmahIOExtensions.cs
@@ -25,7 +25,7 @@ namespace Serilog
     public static class LoggerConfigurationElmahIOExtensions
     {
         /// <summary>
-        /// Adds a sink that writes log events to the elmah.io webservice.
+        /// Adds a sink that writes log events to the elmah.io webservice. By default it will only send errors to elmah.io as it is primarily used for error tracking.
         /// </summary>
         /// <param name="loggerConfiguration">The logger configuration.</param>
         /// <param name="logId">The log id as found on the elmah.io website.</param>
@@ -36,7 +36,7 @@ namespace Serilog
         public static LoggerConfiguration ElmahIO(
             this LoggerSinkConfiguration loggerConfiguration,
              Guid logId,
-            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Error,
             IFormatProvider formatProvider = null)
         {
             if (loggerConfiguration == null) throw new ArgumentNullException("loggerConfiguration");
