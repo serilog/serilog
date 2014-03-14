97df1c3dc8a203c78e93c0cbbdfa0bc4e6f0d7b2
Michiel van Oudheusden <m.vanoudheusden@gmail.com>
2014-03-15 00:09:30 +0100

    Added ElasticSearch sink and sample project.

---
+++ sample/ElasticSearchSample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace ElasticSearchSample
+{
+    class Position
+    {
+        public double Lat { get; set; }
+        public double Long { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+       
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.ColoredConsole()
+                .WriteTo.ElasticSearch()
+                .CreateLogger();
+
+            Log.Verbose("This app, {ExeName}, demonstrates the basics of using Serilog", "Demo.exe");
+
+            ProcessInput(new Position { Lat = 24.7, Long = 132.2 });
+            ProcessInput(new Position { Lat = 24.71, Long = 132.15 });
+            ProcessInput(new Position { Lat = 24.72, Long = 132.2 });
+
+            const int failureCount = 3;
+            Log.Warning("Exception coming up because of {FailureCount} failures...", failureCount);
+
+            try
+            {
+                DoBad();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "There's those {FailureCount} failures", failureCount);
+            }
+
+            Log.Fatal("That's all folks - and all done using {WorkingSet} bytes of RAM", Environment.WorkingSet);
+            Console.ReadKey(true);
+        }
+
+        static void DoBad()
+        {
+            throw new InvalidOperationException("Everything's broken!");
+        }
+
+        static readonly Random Rng = new Random();
+
+        static void ProcessInput(Position sensorInput)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Log.Debug("Processing some input on {MachineName}...", Environment.MachineName);
+            Thread.Sleep(Rng.Next(0, 100));
+            sw.Stop();
+
+            Log.Information("Processed {@SensorInput} in {Time:000} ms", sensorInput, sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ src/Serilog.Sinks.ElasticSearch/LoggerConfigurationElasticSearchExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Events;
+// Copyright 2014 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using Serilog.Sinks.ElasticSearch;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Adds the WriteTo.RavenDB() extension method to <see cref="LoggerConfiguration"/>.
+    /// </summary>
+    public static class LoggerConfigurationElasticSearchExtensions
+    {
+        /// <summary>
+        /// Adds a sink that writes log events as documents to an ElasticSearch index. Make sure to add a template to ElasticSearch
+        /// like the one found here: https://github.com/elasticsearch/logstash/blob/v1.3.1/lib/logstash/outputs/elasticsearch/elasticsearch-template.json
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="indexFormat">The index format where the events are send to. It defaults to the logstash index per day format.</param>
+        /// <param name="server">The server where ElasticSearch is running. When null it falls back to http://localhost:9200</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="batchPostingLimit">The maximum number of events to post in a single batch.</param>
+        /// <param name="period">The time to wait between checking for event batches.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        public static LoggerConfiguration ElasticSearch(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string indexFormat = "logstash-{0:yyyy.MM.dd}",
+            Uri server = null,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            int batchPostingLimit = ElasticSearchSink.DefaultBatchPostingLimit,
+            TimeSpan? period = null,
+            IFormatProvider formatProvider = null)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException("loggerConfiguration");
+
+            var defaultedPeriod = period ?? ElasticSearchSink.DefaultPeriod;
+            if (server == null)
+                server = new Uri("http://localhost:9200");
+
+            return loggerConfiguration.Sink(
+                new ElasticSearchSink(server, indexFormat, batchPostingLimit, defaultedPeriod, formatProvider),
+                restrictedToMinimumLevel);
+        }
+    }
+}
