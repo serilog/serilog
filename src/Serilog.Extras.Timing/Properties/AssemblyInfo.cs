2dc15e657a70683daff36a1acf883801ebacf15d
Michiel van Oudheusden <m.vanoudheusden@aerdata.com>
2014-03-25 00:25:32 +0100

    Added a timing extension as described in issue #20. For now it is a very basic implementation.

---
+++ src/Serilog.Extras.Timing/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Serilog.Extras.Timing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
