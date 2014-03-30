4f234a6156a15e7fa96fda0d471f0a87ad03ba4a
Michiel van Oudheusden <m.vanoudheusden@gmail.com>
2014-03-30 13:59:55 +0200

    Added a logentries sink.

---
+++ src/Serilog.Sinks.Logentries/Sinks/Logentries/LeClient.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Serilog.Sinks.Logentries
+{
+    class LeClient
+    {
+        // Logentries API server address. 
+        protected const String LeApiUrl = "api.logentries.com";
+
+        // Port number for token logging on Logentries API server. 
+        protected const int LeApiTokenPort = 10000;
+
+        // Port number for TLS encrypted token logging on Logentries API server 
+        protected const int LeApiTokenTlsPort = 20000;
+
+        // Port number for HTTP PUT logging on Logentries API server. 
+        protected const int LeApiHttpPort = 80;
+
+        // Port number for SSL HTTP PUT logging on Logentries API server. 
+        protected const int LeApiHttpsPort = 443;
+
+        // Logentries API server certificate. 
+        protected static readonly X509Certificate2 LeApiServerCertificate =
+            new X509Certificate2(Encoding.UTF8.GetBytes(
+                @"-----BEGIN CERTIFICATE-----
+MIIFSjCCBDKgAwIBAgIDBQMSMA0GCSqGSIb3DQEBBQUAMGExCzAJBgNVBAYTAlVT
+MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMR0wGwYDVQQLExREb21haW4gVmFsaWRh
+dGVkIFNTTDEbMBkGA1UEAxMSR2VvVHJ1c3QgRFYgU1NMIENBMB4XDTEyMDkxMDE5
+NTI1N1oXDTE2MDkxMTIxMjgyOFowgcExKTAnBgNVBAUTIEpxd2ViV3RxdzZNblVM
+ek1pSzNiL21hdktiWjd4bEdjMRMwEQYDVQQLEwpHVDAzOTM4NjcwMTEwLwYDVQQL
+EyhTZWUgd3d3Lmdlb3RydXN0LmNvbS9yZXNvdXJjZXMvY3BzIChjKTEyMS8wLQYD
+VQQLEyZEb21haW4gQ29udHJvbCBWYWxpZGF0ZWQgLSBRdWlja1NTTChSKTEbMBkG
+A1UEAxMSYXBpLmxvZ2VudHJpZXMuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
+MIIBCgKCAQEAxcmFqgE2p6+N9lM2GJhe8bNUO0qmcw8oHUVrsneeVA66hj+qKPoJ
+AhGKxC0K9JFMyIzgPu6FvuVLahFZwv2wkbjXKZLIOAC4o6tuVb4oOOUBrmpvzGtL
+kKVN+sip1U7tlInGjtCfTMWNiwC4G9+GvJ7xORgDpaAZJUmK+4pAfG8j6raWgPGl
+JXo2hRtOUwmBBkCPqCZQ1mRETDT6tBuSAoLE1UMlxWvMtXCUzeV78H+2YrIDxn/W
+xd+eEvGTSXRb/Q2YQBMqv8QpAlarcda3WMWj8pkS38awyBM47GddwVYBn5ZLEu/P
+DiRQGSmLQyFuk5GUdApSyFETPL6p9MfV4wIDAQABo4IBqDCCAaQwHwYDVR0jBBgw
+FoAUjPTZkwpHvACgSs5LdW6gtrCyfvwwDgYDVR0PAQH/BAQDAgWgMB0GA1UdJQQW
+MBQGCCsGAQUFBwMBBggrBgEFBQcDAjAdBgNVHREEFjAUghJhcGkubG9nZW50cmll
+cy5jb20wQQYDVR0fBDowODA2oDSgMoYwaHR0cDovL2d0c3NsZHYtY3JsLmdlb3Ry
+dXN0LmNvbS9jcmxzL2d0c3NsZHYuY3JsMB0GA1UdDgQWBBRaMeKDGSFaz8Kvj+To
+j7eMOtT/zTAMBgNVHRMBAf8EAjAAMHUGCCsGAQUFBwEBBGkwZzAsBggrBgEFBQcw
+AYYgaHR0cDovL2d0c3NsZHYtb2NzcC5nZW90cnVzdC5jb20wNwYIKwYBBQUHMAKG
+K2h0dHA6Ly9ndHNzbGR2LWFpYS5nZW90cnVzdC5jb20vZ3Rzc2xkdi5jcnQwTAYD
+VR0gBEUwQzBBBgpghkgBhvhFAQc2MDMwMQYIKwYBBQUHAgEWJWh0dHA6Ly93d3cu
+Z2VvdHJ1c3QuY29tL3Jlc291cmNlcy9jcHMwDQYJKoZIhvcNAQEFBQADggEBAAo0
+rOkIeIDrhDYN8o95+6Y0QhVCbcP2GcoeTWu+ejC6I9gVzPFcwdY6Dj+T8q9I1WeS
+VeVMNtwJt26XXGAk1UY9QOklTH3koA99oNY3ARcpqG/QwYcwaLbFrB1/JkCGcK1+
+Ag3GE3dIzAGfRXq8fC9SrKia+PCdDgNIAFqe+kpa685voTTJ9xXvNh7oDoVM2aip
+v1xy+6OfZyGudXhXag82LOfiUgU7hp+RfyUG2KXhIRzhMtDOHpyBjGnVLB0bGYcC
+566Nbe7Alh38TT7upl/O5lA29EoSkngtUWhUnzyqYmEMpay8yZIV4R9AuUk2Y4HB
+kAuBvDPPm+C0/M4RLYs=
+-----END CERTIFICATE-----"));
+
+        public LeClient(bool useHttpPut, bool useSsl)
+        {
+            m_UseSsl = useSsl;
+            if (!m_UseSsl)
+                m_TcpPort = useHttpPut ? LeApiHttpPort : LeApiTokenPort;
+            else
+                m_TcpPort = useHttpPut ? LeApiHttpsPort : LeApiTokenTlsPort;
+        }
+
+        private bool m_UseSsl = false;
+        private int m_TcpPort;
+        private TcpClient m_Client = null;
+        private Stream m_Stream = null;
+        private SslStream m_SslStream = null;
+
+        private Stream ActiveStream
+        {
+            get
+            {
+                return m_UseSsl ? m_SslStream : m_Stream;
+            }
+        }
+
+        public void Connect()
+        {
+            m_Client = new TcpClient(LeApiUrl, m_TcpPort);
+            m_Client.NoDelay = true;
+
+            m_Stream = m_Client.GetStream();
+
+            if (m_UseSsl)
+            {
+                m_SslStream = new SslStream(m_Stream, false, (sender, cert, chain, errors) => cert.GetCertHashString() == LeApiServerCertificate.GetCertHashString());
+                m_SslStream.AuthenticateAsClient(LeApiUrl);
+            }
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            ActiveStream.Write(buffer, offset, count);
+        }
+
+        public void Flush()
+        {
+            ActiveStream.Flush();
+        }
+
+        public void Close()
+        {
+            if (m_Client != null)
+            {
+                try
+                {
+                    m_Client.Close();
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
