bc913397d2162dbdf52e4568b928da5ca8ef9f46
Damian Hickey <dhickey@gmail.com>
2014-04-13 21:36:58 +0200

    Serilog.Extras.MSOwin: Allow specifying of the context property name.

--- src/Serilog.Extras.MSOwin/Extras/MSOwin/AppBuilderExtensions.cs
+++ src/Serilog.Extras.MSOwin/Extras/MSOwin/AppBuilderExtensions.cs
@@ -5,9 +5,15 @@ namespace Serilog.Extras.MSOwin
 {
     public static class AppBuilderExtensions
     {
-        public static IAppBuilder UseSerilogRequestContext(this IAppBuilder app)
+        /// <summary>
+        /// Open a nested diagnostic context for each request allowing the correlation of log messages per request.
+        /// </summary>
+        /// <param name="app">The IAppBuilder passed to your configuration method</param>
+        /// <param name="propertyName">The property name the request Id is associated with. Defaut is <see cref="RequestContextMiddleware.DefaultRequestIdPropertyName"/></param>
+        /// <returns>The original app parameter</returns>
+        public static IAppBuilder UseSerilogRequestContext(this IAppBuilder app, string propertyName = RequestContextMiddleware.DefaultRequestIdPropertyName)
         {
-            return app.Use(typeof(RequestContextMiddleware));
+            return app.Use(typeof(RequestContextMiddleware), new object[]{ propertyName });
         }
     }
 }
