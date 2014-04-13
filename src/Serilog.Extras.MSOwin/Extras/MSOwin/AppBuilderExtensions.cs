470f89b19b6b63ccc33c6f1e55ccd4fa02cf28b3
Damian Hickey <dhickey@gmail.com>
2014-04-13 21:18:48 +0200

    Serilog.Extras.MSOwin: Add a middleware that opens a nested context property to allow correlation of log messages from an owin pipeline.

---
+++ src/Serilog.Extras.MSOwin/Extras/MSOwin/AppBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+// ReSharper disable once CheckNamespace
+namespace Serilog.Extras.MSOwin
+{
+    public static class AppBuilderExtensions
+    {
+        public static IAppBuilder UseSerilogRequestContext(this IAppBuilder app)
+        {
+            return app.Use(typeof(RequestContextMiddleware));
+        }
+    }
+}
---
+++ src/Serilog.Extras.MSOwin/Extras/MSOwin/RequestContextMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Serilog.Context;
+
+namespace Serilog.Extras.MSOwin
+{
+    public class RequestContextMiddleware
+    {
+        public const string RequestIdPropertyName = "RequestId";
+
+        private readonly Func<IDictionary<string, object>, Task> _next;
+
+        public RequestContextMiddleware(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+            if (next == null)
+            {
+                throw new ArgumentNullException("next");
+            }
+        }
+
+        public async Task Invoke(IDictionary<string, object> enviroment)
+        {
+            // There is not yet a standard way to uniquely identify and correlate an owin request
+            // ... hence 'RequestId' https://github.com/owin/owin/issues/21
+            using (LogContext.PushProperty(RequestIdPropertyName, Guid.NewGuid()))
+            {
+                await _next(enviroment);
+            }
+        }
+    }
+}
