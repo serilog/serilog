18b2bc5a27c45049e72c9dae59ef10fab10ddbde
Alex Russak <abrussak@gmail.com>
2014-05-12 12:16:17 -0700

    Added class to handle various Email connection settings.
    Office365 required me setting the Port to another value and also
    enabling SSL.
    
    Added a new class 'EmailConnectionInfo' that contains properties used by
    the sink instead of direct parameters.
    Original extension method still works and now just constructs a
    connection info and passes that through.

--- src/Serilog.Sinks.Email/LoggerConfigurationEmailExtensions.cs
+++ src/Serilog.Sinks.Email/LoggerConfigurationEmailExtensions.cs
@@ -46,8 +46,8 @@ namespace Serilog
         /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
         public static LoggerConfiguration Email(
             this LoggerSinkConfiguration loggerConfiguration,
-            string fromEmail, 
-            string toEmail, 
+            string fromEmail,
+            string toEmail,
             string mailServer,
             ICredentialsByHost networkCredential,
             string outputTemplate = DefaultOutputTemplate,
@@ -61,12 +61,47 @@ namespace Serilog
             if (toEmail == null) throw new ArgumentNullException("toEmail");
             if (mailServer == null) throw new ArgumentNullException("mailServer");
 
-            var defaultedPeriod = period ?? EmailSink.DefaultPeriod;
+            
+            var connectionInfo = new EmailConnectionInfo
+            {
+                FromEmail = fromEmail,
+                ToEmail = toEmail,
+                MailServer = mailServer,
+                NetworkCredentails = networkCredential
+            };
 
+            return Email(loggerConfiguration, connectionInfo, outputTemplate, restrictedToMinimumLevel, batchPostingLimit, period, formatProvider);
+        }
+
+        /// <summary>
+        /// Adds a sink that sends log events via email.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="connectionInfo">The connection info used for </param>
+        /// <param name="outputTemplate">A message template describing the format used to write to the sink.
+        /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="batchPostingLimit">The maximum number of events to post in a single batch.</param>
+        /// <param name="period">The time to wait between checking for event batches.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        public static LoggerConfiguration Email(
+            this LoggerSinkConfiguration loggerConfiguration,
+            EmailConnectionInfo connectionInfo,
+            string outputTemplate = DefaultOutputTemplate,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            int batchPostingLimit = EmailSink.DefaultBatchPostingLimit,
+            TimeSpan? period = null,
+            IFormatProvider formatProvider = null)
+        {
+            if (connectionInfo == null) throw new ArgumentNullException("connectionInfo");
+
+            var defaultedPeriod = period ?? EmailSink.DefaultPeriod;
             var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
 
             return loggerConfiguration.Sink(
-                new EmailSink(fromEmail, toEmail, mailServer, networkCredential, batchPostingLimit, defaultedPeriod, formatter),
+                new EmailSink(connectionInfo, batchPostingLimit, defaultedPeriod, formatter),
                 restrictedToMinimumLevel);
         }
     }
