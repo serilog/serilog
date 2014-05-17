7d1456e1c80782bd91bba50554f432a6d021f2d8
Simon Cropp <simon.cropp@gmail.com>
2014-05-17 12:12:39 +1000

    better variable naming

--- src/Serilog/Formatting/Json/JsonFormatter.cs
+++ src/Serilog/Formatting/Json/JsonFormatter.cs
@@ -119,10 +119,10 @@ namespace Serilog.Formatting.Json
             if (logEvent.Properties.Count != 0)
             {
                 output.Write(",\"Properties\":{");
-                var pdelim = "";
+                var precedingDelimiter = "";
                 foreach (var property in logEvent.Properties)
                 {
-                    WriteJsonProperty(property.Key, property.Value, ref pdelim, output);
+                    WriteJsonProperty(property.Key, property.Value, ref precedingDelimiter, output);
                 }
                 output.Write("}");
             }
