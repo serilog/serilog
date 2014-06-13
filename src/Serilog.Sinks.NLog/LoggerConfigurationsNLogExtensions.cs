3278a6f98f5fa24aaea07cfd6094f5f7fe73b6af
ruba1987 <russell.j.baker@gmail.com>
2014-06-13 16:22:27 -0400

    Added NLog sink support
    Author:    ruba1987

---
+++ src/Serilog.Sinks.NLog/LoggerConfigurationsNLogExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog.Sinks.NLog;
+using Serilog.Configuration;
+using Serilog.Events;
+
+namespace Serilog
+{
+    public static class LoggerConfigurationsNLogExtensions
+    {
+        public static LoggerConfiguration NLog(
+            this LoggerSinkConfiguration loggerConfiguration,
+            LogEventLevel logLevel = LogEventLevel.Debug,
+            IFormatProvider formatProvider = null)
+        {
+            if (loggerConfiguration == null)
+            {
+                throw new ArgumentNullException("loggerConfiguration");
+            }
+
+            return loggerConfiguration.Sink(new NLogSink(formatProvider), logLevel);
+        }
+    }
+}
---
+++ src/Serilog.Sinks.NLog/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Serilog")]
+[assembly: AssemblyDescription("Serilog Logging Library")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("e157b051-4829-4638-8ea9-ab5a97ac2553")]
