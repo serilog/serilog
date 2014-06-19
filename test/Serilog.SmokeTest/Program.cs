19dbb2bae960de5cfc742663496b02d2f2c6d5b5
nblumhardt <nblumhardt@nblumhardt.com>
2014-06-19 17:06:24 +1000

    Support :u and :w as format specifiers for UPPERCASE and lowercase output respectively; only effective in output templates. Fixes #88 and #151.

---
+++ test/Serilog.SmokeTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Serilog.SmokeTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+
+            var library = typeof(Log).Assembly.GetName();
+            Log.Information("Hello {User} from {@Library}!", Environment.UserName, new { library.Name, Version = library.Version.ToString() });
+        }
+    }
+}
