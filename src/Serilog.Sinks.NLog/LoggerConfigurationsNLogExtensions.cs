4e7036d3c8de063814803e8368f06f4d627f7555
nblumhardt <nblumhardt@nblumhardt.com>
2014-06-30 21:47:58 +1000

    Fixed reference paths in the NLog sink, addressed a couple of minor consistency issues.

--- src/Serilog.Sinks.NLog/LoggerConfigurationsNLogExtensions.cs
+++ src/Serilog.Sinks.NLog/LoggerConfigurationsNLogExtensions.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿// Copyright 2014 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using Serilog.Sinks.NLog;
 using Serilog.Configuration;
 using Serilog.Events;
 
 namespace Serilog
 {
+    /// <summary>
+    /// Adds the WriteTo.NLog() extension method to <see cref="LoggerConfiguration"/>.
+    /// </summary>
     public static class LoggerConfigurationsNLogExtensions
     {
+        /// <summary>
+        /// Adds a sink that writes adapted log events to NLog.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
         public static LoggerConfiguration NLog(
             this LoggerSinkConfiguration loggerConfiguration,
-            LogEventLevel logLevel = LogEventLevel.Debug,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
             IFormatProvider formatProvider = null)
         {
             if (loggerConfiguration == null)
@@ -17,7 +42,7 @@ namespace Serilog
                 throw new ArgumentNullException("loggerConfiguration");
             }
 
-            return loggerConfiguration.Sink(new NLogSink(formatProvider), logLevel);
+            return loggerConfiguration.Sink(new NLogSink(formatProvider), restrictedToMinimumLevel);
         }
     }
 }
