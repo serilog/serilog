a0e2b2374559fd0cdba9550e90684ea4603ae58b
Rob Moore <me@robdmoore.id.au>
2014-08-31 11:13:17 +0800

    Added skeleton of Azure Document DB sink

---
+++ src/Serilog.Sinks.AzureDocumentDb/LoggerConfigurationAzureDocumentDbExtensions.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2014 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Sinks.AzureDocumentDb;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Adds the WriteTo.AzureDocumentDb() extension method to <see cref="LoggerConfiguration"/>.
+    /// </summary>
+    public static class LoggerConfigurationAzureDocumentDbExtensions
+    {
+        /// <summary>
+        /// Adds a sink that writes log events to a Azure DocumentDB table in the provided endpoint.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="endpointUri">The endpoint URI of the document db.</param>
+        /// <param name="authorizationKey">The authorization key of the db.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        public static LoggerConfiguration AzureDocumentDb(
+            this LoggerSinkConfiguration loggerConfiguration,
+            Uri endpointUri,
+            string authorizationKey,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            IFormatProvider formatProvider = null)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException("loggerConfiguration");
+            if (endpointUri == null) throw new ArgumentNullException("endpointUri");
+            if (authorizationKey == null) throw new ArgumentNullException("authorizationKey");
+            return loggerConfiguration.Sink(
+                new AzureDocumentDbSink(endpointUri, authorizationKey, formatProvider),
+                restrictedToMinimumLevel);
+        }
+    }
+}
