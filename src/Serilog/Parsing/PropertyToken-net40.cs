5a4e4a7fa8c15cd51a4abea0fe3f6587c4278c2c
Kristian Hellang <kristian@hellang.com>
2014-10-23 21:12:01 +0200

    Brought net40 project up to par

--- src/Serilog/Parsing/PropertyToken-net40.cs
+++ src/Serilog/Parsing/PropertyToken-net40.cs
@@ -55,8 +55,10 @@ namespace Serilog.Parsing
         /// <param name="format">The format applied to the property, if any.</param>
         /// <param name="alignment">The alignment applied to the property, if any.</param>
         /// <param name="destructuring">The destructuring strategy applied to the property, if any.</param>
+        /// <param name="startIndex">The token's start index in the template.</param>
         /// <exception cref="ArgumentNullException"></exception>
-        public PropertyToken(string propertyName, string rawText, string format = null, Alignment? alignment = null, Destructuring destructuring = Destructuring.Default)
+        public PropertyToken(string propertyName, string rawText, string format = null, Alignment? alignment = null, Destructuring destructuring = Destructuring.Default, int startIndex = 0)
+            : base(startIndex)
         {
             if (propertyName == null)
                 throw new ArgumentNullException("propertyName");
@@ -74,6 +76,14 @@ namespace Serilog.Parsing
             {
                 _position = position;
             }
+        }
+
+        /// <summary>
+        /// The token's length.
+        /// </summary>
+        public override int Length
+        {
+            get { return _rawText.Length; }
         }
 
         /// <summary>
