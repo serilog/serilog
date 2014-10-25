3eb212e5f90c4ba3970f39787ae26b5e4d0cd886
XSockets (@ulfbjo) <codeplanner@gmail.com>
2014-10-25 14:24:11 +0200

    Added XSockets Sink

---
+++ Serilog.Sinks.XSockets/Sinks/XSockets/LogController.cs
@@ -0,0 +1,26 @@
+﻿using XSockets.Core.Common.Utility.Logging;
+using XSockets.Core.XSocket;
+using XSockets.Plugin.Framework.Attributes;
+
+namespace Serilog.Sinks.XSockets.Sinks.XSockets
+{
+    /// <summary>
+    /// The XSockets controller used for dispatching loginformation
+    /// </summary>
+    [XSocketMetadata(PluginAlias = "log")]
+    public class LogController : XSocketController
+    {
+        /// <summary>
+        /// Each client can set the LogEventLevel of interest.
+        /// </summary>
+        public LogEventLevel LogEventLevel { get; set; }
+
+        /// <summary>
+        /// Ctor - By default LogEventLevel will be "Information"
+        /// </summary>
+        public LogController()
+        {
+            this.LogEventLevel = LogEventLevel.Information;
+        }
+    }
+}
