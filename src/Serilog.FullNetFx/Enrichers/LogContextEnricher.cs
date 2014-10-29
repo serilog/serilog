1e62438e805d4c86a530a36353d65c164be32207
kanchanm <kanchanm@microsoft.com>
2014-10-28 19:14:28 -0700

    This review includes - source code changes to port Serilog to support ASP.NET vNext (includes support for console, file, system.diagnostics to start with), smoke test updates. This also includes a temporary Demo project to show all the above features working on vNext. The Serilog.vs2015 solution is created SxS which includes the new projects as well so that people can still open the original solution without issues on VS 2013.

--- src/Serilog.FullNetFx/Enrichers/LogContextEnricher.cs
+++ src/Serilog.FullNetFx/Enrichers/LogContextEnricher.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if !ASPNETCORE50
 using Serilog.Context;
 using Serilog.Core;
 using Serilog.Events;
@@ -26,3 +27,4 @@ namespace Serilog.Enrichers
         }
     }
 }
+#endif
