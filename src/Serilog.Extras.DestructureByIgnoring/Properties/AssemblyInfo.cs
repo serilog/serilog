2f927e5e840bca43e22dac58940c08da35303bb3
Kristian Hellang <kristian@hellang.com>
2014-11-12 02:01:58 +0100

    Added CommonAssemblyInfo.cs

--- src/Serilog.Extras.DestructureByIgnoring/Properties/AssemblyInfo.cs
+++ src/Serilog.Extras.DestructureByIgnoring/Properties/AssemblyInfo.cs
@@ -3,5 +3,3 @@
 [assembly: AssemblyTitle("Serilog.Extras.DestructureByIgnoring")]
 [assembly: AssemblyProduct("Serilog Lambda-Driven Destructuring Support")]
 [assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
