2cfd69eebb0e231fbf3c9024354ac0e37ea0c4f9
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2014-11-16 14:07:18 +1000

    Use 1.0.0 as the default informational version so it's easy to spot when it might have been un-patched during a build

--- assets/CommonAssemblyInfo.cs
+++ assets/CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.15")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
