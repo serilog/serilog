53fbaa2d10d7c88cfa59f80ecc640c471567eb6f
Marco Braga <marco.braga@gmail.com>
2014-12-01 12:56:50 +0100

    Added check for properties in test

--- test/Serilog.Sinks.AzureTableStorageWithProperties.Tests/AzureTableStorageWithPropertiesSinkTests.cs
+++ test/Serilog.Sinks.AzureTableStorageWithProperties.Tests/AzureTableStorageWithPropertiesSinkTests.cs
@@ -94,10 +94,17 @@ namespace Serilog.Sinks.AzureTableStorage.Tests
 			logger.Information(exception, messageTemplate, "Properties", 1234, ' ');
 
 			var result = table.ExecuteQuery(new TableQuery().Take(1)).First();
+			
+			// Check the presence of same properties as in previous version
 			Assert.AreEqual(messageTemplate, result.Properties["MessageTemplate"].StringValue);
 			Assert.AreEqual("Information", result.Properties["Level"].StringValue);
 			Assert.AreEqual("System.ArgumentException: Some exception", result.Properties["Exception"].StringValue);
 			Assert.AreEqual("\"Properties\" should go in their 1234  ", result.Properties["RenderedMessage"].StringValue);
+
+			// Check the presence of the new properties.
+			Assert.AreEqual("\"Properties\"", result.Properties["Properties"].PropertyAsObject);
+			Assert.AreEqual("1234", result.Properties["Numbered"].StringValue);
+			Assert.AreEqual(" ", result.Properties["Space"].StringValue);
 		}
 	}
 }
