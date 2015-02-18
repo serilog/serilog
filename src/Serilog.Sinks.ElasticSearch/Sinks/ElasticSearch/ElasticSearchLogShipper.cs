87be7b4884bc8ac0e8f97998d2bffc0e77793bd2
Mogens Heller Grabe <mookid8000@gmail.com>
2015-02-18 10:49:27 +0100

    removed Console.WriteLine stuff + added file headers

--- src/Serilog.Sinks.ElasticSearch/Sinks/ElasticSearch/ElasticSearchLogShipper.cs
+++ src/Serilog.Sinks.ElasticSearch/Sinks/ElasticSearch/ElasticSearchLogShipper.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2014 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -168,18 +182,14 @@ namespace Serilog.Sinks.ElasticSearch
 
                         if (count > 0)
                         {
-                            Console.Write("Sending {0}... ", count);
-
                             var response = _client.Bulk(payload);
 
                             if (response.Success)
                             {
-                                Console.WriteLine("OK");
                                 WriteBookmark(bookmark, nextLineBeginsAtOffset, currentFilePath);
                             }
                             else
                             {
-                                Console.WriteLine("boo");
                                 SelfLog.WriteLine("Received failed ElasticSearch shipping result {0}: {1}", response.HttpStatusCode, response.OriginalException);
                             }
                         }
