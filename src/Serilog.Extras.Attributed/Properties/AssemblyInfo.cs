9f3a87a03e5871b7795c6dc1cf809388d36ea9f9
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2015-03-29 15:28:16 +1000

    Removed Serilog.Extras.Attributed, now at https://github.com/destructurama/attributed (see #414)

--- src/Serilog.Extras.Attributed/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Serilog.Extras.Attributed")]
-[assembly: AssemblyProduct("Serilog Attribute-Driven Destructuring Support")]
-[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
