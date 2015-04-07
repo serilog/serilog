fa5f0f5d408310f15fe7d4ccae1ffd3d7a4e7bbb
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2015-04-07 21:33:31 +1000

    Moved Serilog.Extras.Web to https://github.com/serilog-web/classic and Serilog.Extras.MSOwin to https://github.com/serilog-web/owin
    This was a trailing part of #344

--- src/Serilog.Extras.MSOwin/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Serilog.Extras.MSOwin")]
-[assembly: AssemblyDescription("Middleware and logging support for OWIN using Serilog")]
-[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
--- src/Serilog.Extras.Web/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Web;
-using Serilog.Extras.Web;
-
-[assembly: AssemblyTitle("Serilog.Web")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2013")]
-
-[assembly: PreApplicationStartMethod(typeof(ApplicationLifecycleModule), "Register")]
