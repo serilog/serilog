c40cc9110263119716d2cc86661b374a7002edd6
Vitaliy Tsvayer <tsvayer@gmail.com>
2015-05-28 17:22:32 +0300

    backport KeyValuePairSettings and AppSettings to net40

---
+++ src/Serilog/Settings/KeyValuePairs/AssemblyExtensions-net40.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Serilog.Settings.KeyValuePairs
+{
+    /// <summary>
+    /// Backport of .Net 4.5 features 
+    /// </summary>
+    static class AssemblyExtensions
+    {
+        public static IEnumerable<Type> ExportedTypes(this Assembly assembly)
+        {
+            return assembly.GetExportedTypes();
+        }
+    }
+}
---
+++ src/Serilog/Settings/KeyValuePairs/ParameterInfoExtensions-40.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Serilog.Settings.KeyValuePairs
+{
+    /// <summary>
+    /// Backport of .Net 4.5 features
+    /// </summary>
+    static class ParameterInfoExtensions
+    {
+        public static bool HasDefaultValue(this ParameterInfo parameterInfo)
+        {
+            return (parameterInfo.Attributes & ParameterAttributes.HasDefault) != ParameterAttributes.None;
+        }    
+    }
+}
