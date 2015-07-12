984f6af3ddc2200e86d0b8d85b83dc987d03d7cd
Shawn Anderson <sanderson@eye-catcher.com>
2015-07-12 11:10:20 -0400

    Reset CommonAssemblyInfo.cs for Appveyor

--- assets/CommonAssemblyInfo.cs
+++ assets/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
+using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.8")]
-[assembly: AssemblyFileVersion("1.5.8")]
-[assembly: AssemblyInformationalVersion("1.5.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
