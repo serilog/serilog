109c5544ca4f4b4a91623f0f6e8e26cf11356c19
Matthew Erbs <matthew.erbs@merbla.com>
2015-12-05 15:14:16 +1000

    Moved ITextFormatter to abstractions

---
+++ src/Serilog.Abstractions/Formatting/ITextFormatter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2013-2015 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.IO;
+using Serilog.Events;
+
+namespace Serilog.Formatting
+{
+    /// <summary>
+    /// Formats log events in a textual representation.
+    /// </summary>
+    public interface ITextFormatter
+    {
+        /// <summary>
+        /// Format the log event into the output.
+        /// </summary>
+        /// <param name="logEvent">The event to format.</param>
+        /// <param name="output">The output.</param>
+        void Format(LogEvent logEvent, TextWriter output);
+    }
+}
