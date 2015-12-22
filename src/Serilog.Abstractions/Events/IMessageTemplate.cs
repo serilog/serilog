95ee36f1cf3b392d2c1ae9653e183306f2fc8277
Matthew Erbs <matthew.erbs@merbla.com>
2015-12-22 21:21:24 +1000

    Clean up and attempting to get build working again
    Doco on `IMessageTemplate`.  Need to determine a cleaner way to decouple
    `LogEvent` from `MessageTemplate`

--- src/Serilog.Abstractions/Events/IMessageTemplate.cs
+++ src/Serilog.Abstractions/Events/IMessageTemplate.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-
 using Serilog.Parsing;
 
 #if NET40
@@ -29,12 +28,36 @@ namespace Serilog.Events
 {
     public interface IMessageTemplate
     {
-        void Render(IPropertyDictionary properties, TextWriter output, IFormatProvider formatProvider = null);
-
+        /// <summary>
+        /// Convert the message template into a textual message, given the
+        /// properties matching the tokens in the message template.
+        /// </summary>
+        /// <param name="properties">Properties matching template tokens.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>The message created from the template and properties. If the
+        /// properties are mismatched with the template, the template will be
+        /// returned with incomplete substitution.</returns>
         string Render(IPropertyDictionary properties, IFormatProvider formatProvider = null);
 
+        /// <summary>
+        /// Convert the message template into a textual message, given the
+        /// properties matching the tokens in the message template.
+        /// </summary>
+        /// <param name="properties">Properties matching template tokens.</param>
+        /// <param name="output">The message created from the template and properties. If the
+        /// properties are mismatched with the template, the template will be
+        /// returned with incomplete substitution.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        void Render(IPropertyDictionary properties, TextWriter output, IFormatProvider formatProvider = null);
+        
+        /// <summary>
+        /// The tokens parsed from the template.
+        /// </summary>
         IEnumerable<MessageTemplateToken> Tokens { get; }
-
+        
+        /// <summary>
+        /// The raw text describing the template.
+        /// </summary>
         string Text { get; }
     }
 }
