7abaafca5ded90f833153075eef9049246ce9a7a
Matthew Erbs <matthew.erbs@merbla.com>
2016-01-20 13:43:43 +1000

    Moved colored console sink

--- src/Serilog/LoggerConfigurationFullNetFxExtensions.cs
+++ src/Serilog/LoggerConfigurationFullNetFxExtensions.cs
@@ -44,32 +44,6 @@ namespace Serilog
         public const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
         public const long DefaultFileSizeLimitBytes = 1L * 1024 * 1024 * 1024;
         const string DefaultConsoleOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
-          
-        /// <summary>
-        /// Writes log events to <see cref="System.Console"/>, using color to differentiate
-        /// between levels.
-        /// </summary>
-        /// <param name="sinkConfiguration">Logger sink configuration.</param>
-        /// <param name="restrictedToMinimumLevel">The minimum level for
-        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
-        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
-        /// to be changed at runtime.</param>
-        /// <param name="outputTemplate">A message template describing the format used to write to the sink.
-        /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
-        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration ColoredConsole(
-            this LoggerSinkConfiguration sinkConfiguration,
-            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
-            string outputTemplate = DefaultConsoleOutputTemplate,
-            IFormatProvider formatProvider = null,
-            LoggingLevelSwitch levelSwitch = null)
-        {
-            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
-            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
-            return sinkConfiguration.Sink(new ColoredConsoleSink(outputTemplate, formatProvider), restrictedToMinimumLevel, levelSwitch);
-        }
-
          
 #if LOGCONTEXT
         /// <summary>
