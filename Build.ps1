ab4d3c8454c1818de2d94fce26eb7a5fac517241
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-05-17 11:30:32 +1000

    Attempt installing .NET CLI from the appveyor install script; c/o https://github.com/dotnet/cli/issues/2113

--- Build.ps1
+++ Build.ps1
@@ -1,45 +1,6 @@
-$root = $(Get-Item $($MyInvocation.MyCommand.Path)).DirectoryName
-
-function Install-DotNetCli
-{
-    & where.exe dotnet 2>&1 | Out-Null
-    if(($LASTEXITCODE -ne 0) -Or ((Test-Path Env:\APPVEYOR) -eq $true))
-    {
-        Write-Host ".NET CLI not found, installing"
-        &{$Branch='dev';iex ((New-Object net.webclient).DownloadString('https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1'))}
-    }
-}
-
-function Get-DotNetSdkVersion
-{
-    $globalJson = Join-Path $PSScriptRoot "global.json"
-    $jsonData = Get-Content -Path $globalJson -Raw | ConvertFrom-JSON
-    return $jsonData.sdk.version
-}
-
-function Remove-PathVariable
-{
-    param([string] $VariableToRemove)
-    $path = [Environment]::GetEnvironmentVariable("PATH", "User")
-    $newItems = $path.Split(';') | Where-Object { $_.ToString() -inotlike $VariableToRemove }
-    [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "User")
-    $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
-    $newItems = $path.Split(';') | Where-Object { $_.ToString() -inotlike $VariableToRemove }
-    [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "Process")
-}
-
 Push-Location $PSScriptRoot
 
-$dnxVersion = Get-DotNetSdkVersion
-
-# Clean
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
-
-# Remove the installed dotnet from the path and force use of
-# per-user dotnet (which we can upgrade as needed without admin permissions)
-# Remove-PathVariable "*Program Files\dotnet*"
-
-Install-DotNetCli
 
 & dotnet restore
 
