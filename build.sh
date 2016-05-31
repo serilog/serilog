10b7db1423ffcb4290bad7c6913f85c67aebb1fe
Matthew Erbs <matthew.erbs@merbla.com>
2016-05-31 18:52:22 +1000

    Removed the target framework on lib build.

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 dotnet restore
 for path in src/*/project.json; do
-    dirname="$(dirname "${path}")"	
-    dotnet build ${dirname} -f netstandard1.0 -c Release
-    dotnet build ${dirname} -f netstandard1.3 -c Release
+    dirname="$(dirname "${path}")"
+    dotnet build ${dirname} -c Release
+    dotnet build ${dirname} -c Release
 done
 
 for path in test/Serilog.Tests/project.json; do
-    dirname="$(dirname "${path}")"	
+    dirname="$(dirname "${path}")"
     dotnet build ${dirname} -f netcoreapp1.0 -c Release
     dotnet test ${dirname} -f netcoreapp1.0  -c Release
 done
