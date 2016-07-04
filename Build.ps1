efd247864384289938d6ed0ea1ee2e52df51377a
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-07-05 09:22:12 +1000

    Common build script, updated release tagging key.

--- Build.ps1
+++ Build.ps1
@@ -2,27 +2,38 @@ Push-Location $PSScriptRoot
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
-& dotnet restore
+& dotnet restore --no-cache
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$branch-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
-Push-Location src/Serilog
+foreach ($src in ls src/Serilog.*) {
+    Push-Location $src
 
-& dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$revision
-if($LASTEXITCODE -ne 0) { exit 1 }    
+    & dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$suffix
+    if($LASTEXITCODE -ne 0) { exit 1 }    
 
-Pop-Location
-Push-Location test/Serilog.Tests
+    Pop-Location
+}
 
-& dotnet test -c Release
-if($LASTEXITCODE -ne 0) { exit 2 }
 
-Pop-Location
-Push-Location test/Serilog.PerformanceTests
+foreach ($test in ls test/*.PerformanceTests) {
+    Push-Location $test
 
-#  Currently no running Perf Tests in CI.
-& dotnet build -c Release
-if($LASTEXITCODE -ne 0) { exit 2 }
+    & dotnet build -c Release
+    if($LASTEXITCODE -ne 0) { exit 2 }
 
-Pop-Location
+    Pop-Location
+}
+
+foreach ($test in ls test/*.Tests) {
+    Push-Location $test
+
+    & dotnet test -c Release
+    if($LASTEXITCODE -ne 0) { exit 3 }
+
+    Pop-Location
+}
+
 Pop-Location
