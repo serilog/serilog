df69837d6ce6e2c09597b58726194e5d7f294c19
Adam Chester <adamchester@gmail.com>
2017-03-05 08:11:55 +1000

    Fix travis build, remove OpenCover

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+export DOTNET_INSTALL_DIR="$PWD/.dotnetcli"
+DotnetCliVersion=${CLI_VERSION:="1.0.0-rc4-004771"}
+install_script_url=https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
+curl -sSL $install_script_url | bash /dev/stdin --version "$DotnetCliVersion" --install-dir "$DOTNET_INSTALL_DIR"
+export PATH="$DOTNET_INSTALL_DIR:$PATH"
+
+dotnet --info
 dotnet restore
+
 for path in src/**/*.csproj; do
     dotnet build -c Release ${path}
 done
