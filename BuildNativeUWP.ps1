813828dc9a2e9e3bec9069594dc6dc65fdfdc596
Sergey Komisarchik <komisarchik@media-saturn.com>
2017-07-03 14:15:05 +0300

    enable .net native toolchain for ci

---
+++ BuildNativeUWP.ps1
@@ -0,0 +1,17 @@
+param(
+    [String] $customLogger = "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll",
+    [String] $proj = "test/Serilog.UWP"
+)
+
+echo "build: running .NET Native toolchain for UWP test project"
+
+$msbuild = $(cmd /c where /R 'C:\Program Files (x86)\Microsoft Visual Studio\2017' msbuild.exe | select -First 1)
+
+if ($customLogger) {
+    & $msbuild $proj /logger:"$customLogger" /verbosity:minimal /t:Restore 
+    & $msbuild $proj /logger:"$customLogger" /verbosity:minimal /t:Build /p:Configuration=Release
+}
+else {
+    & $msbuild $proj /verbosity:minimal /t:Restore 
+    & $msbuild $proj /verbosity:minimal /t:Build /p:Configuration=Release
+}
