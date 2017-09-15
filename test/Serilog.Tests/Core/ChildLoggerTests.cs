4d9a0f90b976d7ef3aa65170c635a28363c3be66
Thibaud Desodt <tibo.desodt@gmail.com>
2017-09-15 07:21:41 +0200

    Added more tests for simple overrides w/ child loggers

--- test/Serilog.Tests/Core/ChildLoggerTests.cs
+++ test/Serilog.Tests/Core/ChildLoggerTests.cs
@@ -1,4 +1,6 @@
-﻿using Serilog.Events;
+﻿using Serilog.Core;
+using Serilog.Core.Sinks;
+using Serilog.Events;
 using Serilog.Tests.Support;
 using Xunit;
 using static Serilog.Events.LogEventLevel;
@@ -168,5 +170,82 @@ namespace Serilog.Tests.Core
             Assert.Equal(eventShouldGetToChild, evt != null);
         }
 
+
+
+        [Theory]
+        // Visualizing the pipeline from left to right ....
+        //
+        //   Event  --> Root Logger --> Child Logger -> YES or
+        //    lvl       override/lvl    override/levl     NO ?
+        //
+        // numbers are relative to incoming event level
+        // Information + 1 = Warning
+        // Information - 1 = Debug
+        //
+        // Incoming event is Information
+        // with SourceContext Root.N1.N2
+        //
+        // - default case - no overrides
+        [InlineData(null, 0, null, 0, true)]
+        // - root overrides with level lower or equal to event
+        // ... and child logger is out of the way
+        [InlineData("Root", +0, null, +0, true)]
+        [InlineData("Root", -1, null, +0, true)]
+        [InlineData("Root.N1", +0, null, +0, true)]
+        [InlineData("Root.N1", -1, null, +0, true)]
+        [InlineData("Root.N1.N2", +0, null, +0, true)]
+        [InlineData("Root.N1.N2", -1, null, +0, true)]
+        // - root overrides on irrelevant namespaces
+        [InlineData("xx", +1, null, +0, true)]
+        [InlineData("Root.xx", +1, null, +0, true)]
+        [InlineData("Root.N1.xx", +1, null, +0, true)]
+        // - root overrides prevent all processing from children
+        // even though children would happily accept it
+        [InlineData("Root", +1, null, +0, false)]
+        [InlineData("Root", +1, "Root", +0, false)]
+        [InlineData("Root.N1", +1, null, +0, false)]
+        [InlineData("Root.N1", +1, "Root.N1", +0, false)]
+        [InlineData("Root.N1.N2", +1, null, +0, false)]
+        [InlineData("Root.N1.N2", +1, "Root.N1.N2", +0, false)]
+        public void WriteToLoggerWithConfigCallbackMinimumLevelOverrideInheritanceScenarios(
+            string rootOverrideSource,
+            int rootOverrideLevelIncrement,
+            string childOverrideSource,
+            int childOverrideLevelIncrement,
+            bool eventShouldGetToChild)
+        {
+            var incomingEventLevel = Information;
+            var rootOverrideLevel = incomingEventLevel + rootOverrideLevelIncrement;
+            var childOverrideLevel = incomingEventLevel + childOverrideLevelIncrement;
+
+            LogEvent evt = null;
+            var sink = new DelegatingSink(e => evt = e);
+
+            var rootLoggerConfig = new LoggerConfiguration()
+                .MinimumLevel.Is(LevelAlias.Minimum);
+
+            if (rootOverrideSource != null)
+            {
+                rootLoggerConfig.MinimumLevel.Override(rootOverrideSource, rootOverrideLevel);
+            }
+
+            var logger = rootLoggerConfig
+                .WriteTo.Logger(lc =>
+                {
+                    lc.MinimumLevel.Is(LevelAlias.Minimum);
+                    if (childOverrideSource != null)
+                    {
+                        lc.MinimumLevel.Override(childOverrideSource, childOverrideLevel);
+                    }
+                    lc.WriteTo.Sink(sink);
+                })
+                .CreateLogger();
+
+            logger
+                .ForContext(Constants.SourceContextPropertyName, "Root.N1.N2")
+                .Write(Some.LogEvent(level: incomingEventLevel));
+
+            Assert.Equal(eventShouldGetToChild, evt != null);
+        }
     }
 }
