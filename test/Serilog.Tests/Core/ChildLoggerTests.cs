891b7ccd63f06259e2d2fa71dba56d505365d36c
Thibaud Desodt <tibo.desodt@gmail.com>
2017-09-17 15:03:12 +0200

    Reuse test cases between WriteTo.Logger(Action<config>) and WriteTo.Logger(ILogger)

--- test/Serilog.Tests/Core/ChildLoggerTests.cs
+++ test/Serilog.Tests/Core/ChildLoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using Serilog.Core;
+﻿using System.Collections.Generic;
+using Serilog.Core;
 using Serilog.Events;
 using Serilog.Tests.Support;
 using Xunit;
@@ -9,58 +10,67 @@ namespace Serilog.Tests.Core
     public class ChildLoggerTests
     {
 
+        public static IEnumerable<object[]> GetMinimumLevelInheritanceTestCases()
+        {
+            // Visualizing the pipeline from left to right ....
+            //
+            //   Event  --> Root Logger --> restrictedTo --> Child Logger -> YES or
+            //    lvl        min lvl           param            min lvl       NO ?
+            //
+            object[] T(LogEventLevel el, int? rl, int? rt, int? cl, bool r)
+            {
+                return new object[]{ el, rl, rt, cl, r };
+            }
+            // numbers are relative to incoming event level
+            // Information + 1 = Warning
+            // Information - 1 = Debug
+            // Information + null = default
+            //
+            // - default case - nothing specified
+            // equivalent to "Information+ allowed"
+            yield return T(Verbose,     null, null, null, false);
+            yield return T(Debug,       null, null, null, false);
+            yield return T(Information, null, null, null, true);
+            yield return T(Warning,     null, null, null, true);
+            yield return T(Error,       null, null, null, true);
+            yield return T(Fatal,       null, null, null, true);
+                                    
+            // - cases where event level is high enough all along the pipeline
+            //                  e       -->  --> -->  = OK
+            yield return T(Verbose,     +0, +0, +0, true);
+            yield return T(Debug,       +0, +0, +0, true);
+            yield return T(Information, +0, +0, +0, true);
+            yield return T(Warning,     +0, +0, +0, true);
+            yield return T(Error,       +0, +0, +0, true);
+            yield return T(Fatal,       +0, +0, +0, true);
+
+            // - cases where event is blocked by root minimum level
+            //                 e        -x>  -   -   = NO
+            yield return T(Verbose,     +1, +0, +0, false);
+            yield return T(Debug,       +1, +0, +0, false);
+            yield return T(Information, +1, +0, +0, false);
+            yield return T(Warning,     +1, +0, +0, false);
+            yield return T(Error,       +1, +0, +0, false);
+
+            // - cases where event is blocked by param restrictedToMinimumLevel
+            //                 e        --> -x>  -   = NO
+            yield return T(Verbose,     +0, +1, +0, false);
+            yield return T(Debug,       +0, +1, +0, false);
+            yield return T(Information, +0, +1, +0, false);
+            yield return T(Warning,     +0, +1, +0, false);
+            yield return T(Error,       +0, +1, +0, false);
+
+            // - cases where event is blocked by child minimum level
+            //                 e        --> --> -x>  = NO
+            yield return T(Verbose,     +0, +0, +1, false);
+            yield return T(Debug,       +0, +0, +1, false);
+            yield return T(Information, +0, +0, +1, false);
+            yield return T(Warning,     +0, +0, +1, false);
+            yield return T(Error,       +0, +0, +1, false);
+        }
+
         [Theory]
-        // Visualizing the pipeline from left to right ....
-        //
-        //   Event  --> Root Logger --> restrictedTo --> Child Logger -> YES or
-        //    lvl        min lvl           param            min lvl       NO ?
-        //
-        // numbers are relative to incoming event level
-        // Information + 1 = Warning
-        // Information - 1 = Debug
-        // Information + null = default
-        //
-        // - default case - nothing specified
-        // equivalent to "Information+ allowed"
-        [InlineData(Verbose,     null, null, null, false)]
-        [InlineData(Debug,       null, null, null, false)]
-        [InlineData(Information, null, null, null, true)]
-        [InlineData(Warning,     null, null, null, true)]
-        [InlineData(Error,       null, null, null, true)]
-        [InlineData(Fatal,       null, null, null, true)]
-
-        // - cases where event level is high enough all along the pipeline
-        //              e       -->  --> -->  = OK
-        [InlineData(Verbose,     +0, +0, +0, true)]
-        [InlineData(Debug,       +0, +0, +0, true)]
-        [InlineData(Information, +0, +0, +0, true)]
-        [InlineData(Warning,     +0, +0, +0, true)]
-        [InlineData(Error,       +0, +0, +0, true)]
-        [InlineData(Fatal,       +0, +0, +0, true)]
-
-        // - cases where event is blocked by root minimum level
-        //              e        -x>  -   -   = NO
-        [InlineData(Verbose,     +1, +0, +0, false)]
-        [InlineData(Debug,       +1, +0, +0, false)]
-        [InlineData(Information, +1, +0, +0, false)]
-        [InlineData(Warning,     +1, +0, +0, false)]
-        [InlineData(Error,       +1, +0, +0, false)]
-
-        // - cases where event is blocked by param restrictedToMinimumLevel
-        //              e        --> -x>  -   = NO
-        [InlineData(Verbose,     +0, +1, +0, false)]
-        [InlineData(Debug,       +0, +1, +0, false)]
-        [InlineData(Information, +0, +1, +0, false)]
-        [InlineData(Warning,     +0, +1, +0, false)]
-        [InlineData(Error,       +0, +1, +0, false)]
-
-        // - cases where event is blocked by child minimum level
-        //              e        --> --> -x>  = NO
-        [InlineData(Verbose,     +0, +0, +1, false)]
-        [InlineData(Debug,       +0, +0, +1, false)]
-        [InlineData(Information, +0, +0, +1, false)]
-        [InlineData(Warning,     +0, +0, +1, false)]
-        [InlineData(Error,       +0, +0, +1, false)]
+        [MemberData(nameof(GetMinimumLevelInheritanceTestCases))]
         public void WriteToLoggerWithConfigCallbackMinimumLevelInheritanceScenarios(
             LogEventLevel eventLevel,
             int? rootMinimumLevelIncrement,
@@ -96,57 +106,7 @@ namespace Serilog.Tests.Core
         }
 
         [Theory]
-         // Visualizing the pipeline from left to right ....
-        //
-        //   Event  --> Root Logger --> restrictedTo --> Child Logger -> YES or
-        //    lvl        min lvl           param            min lvl       NO ?
-        //
-        // numbers are relative to incoming event level
-        // Information + 1 = Warning
-        // Information - 1 = Debug
-        // Information + null = default
-        //
-        // - default case - nothing specified
-        // equivalent to "Information+ allowed"
-        [InlineData(Verbose,     null, null, null, false)]
-        [InlineData(Debug,       null, null, null, false)]
-        [InlineData(Information, null, null, null, true)]
-        [InlineData(Warning,     null, null, null, true)]
-        [InlineData(Error,       null, null, null, true)]
-        [InlineData(Fatal,       null, null, null, true)]
-
-        // - cases where event level is high enough all along the pipeline
-        //              e       -->  --> -->  = OK
-        [InlineData(Verbose,     +0, +0, +0, true)]
-        [InlineData(Debug,       +0, +0, +0, true)]
-        [InlineData(Information, +0, +0, +0, true)]
-        [InlineData(Warning,     +0, +0, +0, true)]
-        [InlineData(Error,       +0, +0, +0, true)]
-        [InlineData(Fatal,       +0, +0, +0, true)]
-
-        // - cases where event is blocked by root minimum level
-        //              e        -x>  -   -   = NO
-        [InlineData(Verbose,     +1, +0, +0, false)]
-        [InlineData(Debug,       +1, +0, +0, false)]
-        [InlineData(Information, +1, +0, +0, false)]
-        [InlineData(Warning,     +1, +0, +0, false)]
-        [InlineData(Error,       +1, +0, +0, false)]
-
-        // - cases where event is blocked by param restrictedToMinimumLevel
-        //              e        --> -x>  -   = NO
-        [InlineData(Verbose,     +0, +1, +0, false)]
-        [InlineData(Debug,       +0, +1, +0, false)]
-        [InlineData(Information, +0, +1, +0, false)]
-        [InlineData(Warning,     +0, +1, +0, false)]
-        [InlineData(Error,       +0, +1, +0, false)]
-
-        // - cases where event is blocked by child minimum level
-        //              e        --> --> -x>  = NO
-        [InlineData(Verbose,     +0, +0, +1, false)]
-        [InlineData(Debug,       +0, +0, +1, false)]
-        [InlineData(Information, +0, +0, +1, false)]
-        [InlineData(Warning,     +0, +0, +1, false)]
-        [InlineData(Error,       +0, +0, +1, false)]
+        [MemberData(nameof(GetMinimumLevelInheritanceTestCases))]
         public void WriteToLoggerMinimumLevelInheritanceScenarios(
             LogEventLevel eventLevel,
             int? rootMinimumLevelIncrement,
@@ -183,61 +143,68 @@ namespace Serilog.Tests.Core
             }
         }
 
-
+        public static IEnumerable<object[]> GetMinimumLevelOverrideInheritanceTestCases()
+        {
+            // Visualizing the pipeline from left to right ....
+            //
+            //   Event  --> Root Logger --> Child Logger -> YES or
+            //    lvl       override/lvl    override/levl     NO ?
+            //
+            object[] T(string rs, int? rl, string cs, int? cl, bool r)
+            {
+                return new object[] { rs, rl, cs, cl, r };
+            }
+            // numbers are relative to incoming event level
+            // Information + 1 = Warning
+            // Information - 1 = Debug
+            //
+            // Incoming event is Information
+            // with SourceContext Root.N1.N2
+            //
+            // - default case - no overrides
+            yield return T(null, 0, null, 0, true);
+            // - root overrides with level lower or equal to event
+            // ... and child logger is out of the way
+            yield return T("Root", +0, null, +0, true);
+            yield return T("Root", -1, null, +0, true);
+            yield return T("Root.N1", +0, null, +0, true);
+            yield return T("Root.N1", -1, null, +0, true);
+            yield return T("Root.N1.N2", +0, null, +0, true);
+            yield return T("Root.N1.N2", -1, null, +0, true);
+            // - root overrides on irrelevant namespaces
+            yield return T("xx", +1, null, +0, true);
+            yield return T("Root.xx", +1, null, +0, true);
+            yield return T("Root.N1.xx", +1, null, +0, true);
+            // - child overrides on irrelevant namespaces
+            yield return T(null, +0, "xx", +1, true);
+            yield return T(null, +0, "Root.xx", +1, true);
+            yield return T(null, +1, "Root.N1.xx", +1, true);
+            // - root overrides prevent all processing from children
+            // even though children would happily accept it
+            yield return T("Root", +1, null, +0, false);
+            yield return T("Root", +1, "Root", +0, false);
+            yield return T("Root.N1", +1, null, +0, false);
+            yield return T("Root.N1", +1, "Root.N1", +0, false);
+            yield return T("Root.N1.N2", +1, null, +0, false);
+            yield return T("Root.N1.N2", +1, "Root.N1.N2", +0, false);
+            // - no root overrides but children has its own
+            yield return T(null, +0, "Root", +1, false);
+            yield return T(null, +0, "Root.N1", +1, false);
+            yield return T(null, +0, "Root.N1.N2", +1, false);
+            // - root overrides let it through but child rejects it
+            yield return T("Root", +0, "Root", +1, false);
+            yield return T("Root.N1", +0, "Root", +1, false);
+            yield return T("Root.N1.N2", +0, "Root", +1, false);
+            yield return T("Root", +0, "Root.N1", +1, false);
+            yield return T("Root.N1", +0, "Root.N1", +1, false);
+            yield return T("Root.N1.N2", +0, "Root.N1", +1, false);
+            yield return T("Root", +0, "Root.N1.N2", +1, false);
+            yield return T("Root.N1", +0, "Root.N1.N2", +1, false);
+            yield return T("Root.N1.N2", +0, "Root.N1.N2", +1, false);
+    }
 
         [Theory]
-        // Visualizing the pipeline from left to right ....
-        //
-        //   Event  --> Root Logger --> Child Logger -> YES or
-        //    lvl       override/lvl    override/levl     NO ?
-        //
-        // numbers are relative to incoming event level
-        // Information + 1 = Warning
-        // Information - 1 = Debug
-        //
-        // Incoming event is Information
-        // with SourceContext Root.N1.N2
-        //
-        // - default case - no overrides
-        [InlineData(null, 0, null, 0, true)]
-        // - root overrides with level lower or equal to event
-        // ... and child logger is out of the way
-        [InlineData("Root", +0, null, +0, true)]
-        [InlineData("Root", -1, null, +0, true)]
-        [InlineData("Root.N1", +0, null, +0, true)]
-        [InlineData("Root.N1", -1, null, +0, true)]
-        [InlineData("Root.N1.N2", +0, null, +0, true)]
-        [InlineData("Root.N1.N2", -1, null, +0, true)]
-        // - root overrides on irrelevant namespaces
-        [InlineData("xx", +1, null, +0, true)]
-        [InlineData("Root.xx", +1, null, +0, true)]
-        [InlineData("Root.N1.xx", +1, null, +0, true)]
-        // - child overrides on irrelevant namespaces
-        [InlineData(null, +0, "xx", +1, true)]
-        [InlineData(null, +0, "Root.xx", +1, true)]
-        [InlineData(null, +1, "Root.N1.xx", +1, true)]
-        // - root overrides prevent all processing from children
-        // even though children would happily accept it
-        [InlineData("Root", +1, null, +0, false)]
-        [InlineData("Root", +1, "Root", +0, false)]
-        [InlineData("Root.N1", +1, null, +0, false)]
-        [InlineData("Root.N1", +1, "Root.N1", +0, false)]
-        [InlineData("Root.N1.N2", +1, null, +0, false)]
-        [InlineData("Root.N1.N2", +1, "Root.N1.N2", +0, false)]
-        // - no root overrides but children has its own
-        [InlineData(null, +0, "Root", +1, false)]
-        [InlineData(null, +0, "Root.N1", +1, false)]
-        [InlineData(null, +0, "Root.N1.N2", +1, false)]
-        // - root overrides let it through but child rejects it
-        [InlineData("Root", +0, "Root", +1, false)]
-        [InlineData("Root.N1", +0, "Root", +1, false)]
-        [InlineData("Root.N1.N2", +0, "Root", +1, false)]
-        [InlineData("Root", +0, "Root.N1", +1, false)]
-        [InlineData("Root.N1", +0, "Root.N1", +1, false)]
-        [InlineData("Root.N1.N2", +0, "Root.N1", +1, false)]
-        [InlineData("Root", +0, "Root.N1.N2", +1, false)]
-        [InlineData("Root.N1", +0, "Root.N1.N2", +1, false)]
-        [InlineData("Root.N1.N2", +0, "Root.N1.N2", +1, false)]
+        [MemberData(nameof(GetMinimumLevelOverrideInheritanceTestCases))]
         public void WriteToLoggerWithConfigCallbackMinimumLevelOverrideInheritanceScenarios(
             string rootOverrideSource,
             int rootOverrideLevelIncrement,
@@ -285,61 +252,9 @@ namespace Serilog.Tests.Core
                 Assert.Null(evt);
             }
         }
-
-
+        
         [Theory]
-        // Visualizing the pipeline from left to right ....
-        //
-        //   Event  --> Root Logger --> Child Logger -> YES or
-        //    lvl       override/lvl    override/levl     NO ?
-        //
-        // numbers are relative to incoming event level
-        // Information + 1 = Warning
-        // Information - 1 = Debug
-        //
-        // Incoming event is Information
-        // with SourceContext Root.N1.N2
-        //
-        // - default case - no overrides
-        [InlineData(null, 0, null, 0, true)]
-        // - root overrides with level lower or equal to event
-        // ... and child logger is out of the way
-        [InlineData("Root", +0, null, +0, true)]
-        [InlineData("Root", -1, null, +0, true)]
-        [InlineData("Root.N1", +0, null, +0, true)]
-        [InlineData("Root.N1", -1, null, +0, true)]
-        [InlineData("Root.N1.N2", +0, null, +0, true)]
-        [InlineData("Root.N1.N2", -1, null, +0, true)]
-        // - root overrides on irrelevant namespaces
-        [InlineData("xx", +1, null, +0, true)]
-        [InlineData("Root.xx", +1, null, +0, true)]
-        [InlineData("Root.N1.xx", +1, null, +0, true)]
-        // - child overrides on irrelevant namespaces
-        [InlineData(null, +0, "xx", +1, true)]
-        [InlineData(null, +0, "Root.xx", +1, true)]
-        [InlineData(null, +1, "Root.N1.xx", +1, true)]
-        // - root overrides prevent all processing from children
-        // even though children would happily accept it
-        [InlineData("Root", +1, null, +0, false)]
-        [InlineData("Root", +1, "Root", +0, false)]
-        [InlineData("Root.N1", +1, null, +0, false)]
-        [InlineData("Root.N1", +1, "Root.N1", +0, false)]
-        [InlineData("Root.N1.N2", +1, null, +0, false)]
-        [InlineData("Root.N1.N2", +1, "Root.N1.N2", +0, false)]
-        // - no root overrides but children has its own
-        [InlineData(null, +0, "Root", +1, false)]
-        [InlineData(null, +0, "Root.N1", +1, false)]
-        [InlineData(null, +0, "Root.N1.N2", +1, false)]
-        // - root overrides let it through but child rejects it
-        [InlineData("Root", +0, "Root", +1, false)]
-        [InlineData("Root.N1", +0, "Root", +1, false)]
-        [InlineData("Root.N1.N2", +0, "Root", +1, false)]
-        [InlineData("Root", +0, "Root.N1", +1, false)]
-        [InlineData("Root.N1", +0, "Root.N1", +1, false)]
-        [InlineData("Root.N1.N2", +0, "Root.N1", +1, false)]
-        [InlineData("Root", +0, "Root.N1.N2", +1, false)]
-        [InlineData("Root.N1", +0, "Root.N1.N2", +1, false)]
-        [InlineData("Root.N1.N2", +0, "Root.N1.N2", +1, false)]
+        [MemberData(nameof(GetMinimumLevelOverrideInheritanceTestCases))]
         public void WriteToLoggerMinimumLevelOverrideInheritanceScenarios(
             string rootOverrideSource,
             int rootOverrideLevelIncrement,
