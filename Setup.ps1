df6dc6943f3dc4930a5fcd4d22313fe7dff2b057
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-05-28 13:51:42 +1000

    Make ./build

--- Setup.ps1
+++ Setup.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop"
 
 $RequiredDotnetVersion =  $(cat ./global.json | convertfrom-json).sdk.version
 
+mkdir "./build"
 Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile "./build/installcli.ps1"
 & ./build/installcli.ps1 -InstallDir "$pwd/.dotnetcli" -NoPath -Version $RequiredDotnetVersion
 if ($LASTEXITCODE) { exit 1 }
