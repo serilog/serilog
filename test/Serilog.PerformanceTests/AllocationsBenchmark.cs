8ae8b9e48fd4bbe26dd84e9b60fe3a3b9928f93b
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2019-08-30 19:25:04 -0700

    Code Format

--- test/Serilog.PerformanceTests/AllocationsBenchmark.cs
+++ test/Serilog.PerformanceTests/AllocationsBenchmark.cs
@@ -13,25 +13,29 @@ namespace Serilog.PerformanceTests
     {
         readonly ILogger _logger;
         readonly ILogger _enrichedLogger;
-        readonly LogEvent _emptyEvent;
-        readonly object _dictionaryValue;
-        readonly object _anonymousObject;
-        readonly object _sequence;
-
-        public AllocationsBenchmark()
-        {
-            _logger = new LoggerConfiguration().CreateLogger();
-
-            _enrichedLogger = _logger.ForContext(new PropertyEnricher("Prop", "Value"));
-
-            _emptyEvent = new LogEvent(
+        
+        readonly LogEvent _emptyEvent = new LogEvent(
                 DateTimeOffset.Now,
                 LogEventLevel.Information,
                 null,
                 new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()),
                 Enumerable.Empty<LogEventProperty>());
 
-            _anonymousObject = new
+        readonly object _dictionaryValue = new Dictionary<string, object>
+            {
+                { "Level11", "Val1" },
+                { "Level12", new Dictionary<string, object> {
+                        { "Level21", (int?)42 },
+                        { "Level22", new Dictionary<string, object> {
+                                { "Level31", System.Reflection.BindingFlags.FlattenHierarchy },
+                                { "Level32", new { X = 3, Y = "4", Z = (short?)5 } }
+                            }
+                        }
+                    }
+                }
+            };
+
+        readonly object _anonymousObject = new
             {
                 Level11 = "Val1",
                 Level12 = new
@@ -50,20 +54,13 @@ namespace Serilog.PerformanceTests
                 }
             };
 
-            _dictionaryValue = new Dictionary<string, object> {
-                { "Level11", "Val1" },
-                { "Level12", new Dictionary<string, object> {
-                        { "Level21", (int?)42 },
-                        { "Level22", new Dictionary<string, object> {
-                                { "Level31", System.Reflection.BindingFlags.FlattenHierarchy },
-                                { "Level32", new { X = 3, Y = "4", Z = (short?)5 } }
-                            }
-                        }
-                    }
-                }
-            };
+        readonly object _sequence = new List<object> { "1", 2, (int?)3, "4", (short)5 };
 
-            _sequence = new List<object> { "1", 2, (int?)3, "4", (short)5 };
+        public AllocationsBenchmark()
+        {
+            _logger = new LoggerConfiguration().CreateLogger();
+
+            _enrichedLogger = _logger.ForContext(new PropertyEnricher("Prop", "Value"));
         }
 
         [Benchmark(Baseline = true)]
