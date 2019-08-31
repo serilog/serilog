2c2703f765402fc0ae6c864e3f1d169f496c3773
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2019-08-30 19:05:59 -0700

    Add AlmostRealWorldBenchmark This is a Benchmark to simulate a real application using Serilog

---
+++ test/Serilog.PerformanceTests/AlmostRealWorldBenchmark.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.PerformanceTests.Support;
+
+namespace Serilog.PerformanceTests
+{
+    [MemoryDiagnoser]
+    [MinColumn, MaxColumn]
+    [ClrJob(baseline: true), CoreJob]
+    [ShortRunJob]
+    public class AlmostRealWorldBenchmark
+    {
+        static readonly LoggingLevelSwitch LoggingLevelSwitch = new LoggingLevelSwitch(LogEventLevel.Verbose);
+        static readonly Random Rnd = new Random(42);
+
+        ILogger CreateLog()
+        {
+            return Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Environment", "BenchmarkTester")
+                .Enrich.WithProperty("Application", "Serilog.PerformanceTests.AlmostRealWorldBenchmark")
+                .Enrich.WithProperty("ExecutionId", Guid.NewGuid())
+                .Enrich.WithProperty("ExecutionStartDate", DateTimeOffset.UtcNow)
+                .MinimumLevel.ControlledBy(LoggingLevelSwitch)
+                .WriteTo.Logger(l => l
+                    .Filter.ByExcluding(ev => ev.Properties.ContainsKey("ProgressEntry"))
+                    .WriteTo.Sink(new NullSink())) //To Simulate a FileSink that don't 'records' some LogEvents
+                .WriteTo.Sink(new NullSink()) //To Simulate a Console Sink that show all LogEvents
+                .CreateLogger();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void LogLikeAApp()
+        {
+            var log = CreateLog(); //Always create a new instance of the logger each test to Benchmark the creation with the test.
+            try
+            {
+                log.Debug("App - Start...");
+
+                log.Information("Arguments: {@Args}", new[] { "test", "performance", "-q" });
+                log.Debug("Parsing args.");
+                var execType = ExecutionType.Test;
+
+                var log2 = log.ForContext("Type", execType);
+                log2.Information("Running in {Type} mode", execType);
+
+                RunTest(log2);
+
+                log.Debug("App - Ending...");
+            }
+            finally
+            {
+                (log as IDisposable)?.Dispose();
+            }
+        }
+
+        static void RunTest(ILogger log)
+        {
+            var start = DateTimeOffset.UtcNow;
+            log.Information("Starting Exec of the Test Begin at {StartDateTime}", start);
+
+            using (LogContext.PushProperty("Operation", "Testing"))
+            {
+                var todo = Enumerable.Range(0, 10_000).ToList();
+                var passed = 0;
+                var fail = 0;
+
+                foreach (var i in todo.LogProgress(log))
+                {
+                    using (LogContext.PushProperty("Item", i))
+                    {
+                        log.Verbose("Testing... {ItemNumber}", i);
+
+                        var result = (Rnd.Next(0, 1) == 1);
+                        if (result)
+                        {
+                            passed++;
+                            log.Information("Test Passed.");
+                        }
+                        else
+                        {
+                            fail++;
+                            log.Warning("Test not Passed.");
+                        }
+
+                        log.Verbose("Item Test End");
+                    }
+                }
+                log.Debug("Test End");
+
+                log.Information("Qnt of tests: {QntOfItems}", todo.Count);
+                log.Information("Test Statistics: {@Statistics}", new { Qnt = todo.Count, Passed = passed, Fail = fail });
+            }
+
+            var end = DateTimeOffset.UtcNow;
+            if (log.IsEnabled(LogEventLevel.Information))
+            {
+                log.Information($"Exec ended - Elapsed {(end - start)}");
+            }
+        }
+
+        enum ExecutionType
+        {
+            Test,
+            Build,
+            Run,
+        }
+    }
+}
+
---
+++ test/Serilog.PerformanceTests/Support/LogProgressExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serilog.PerformanceTests.Support
+{
+    public static class LogProgressExtension
+    {
+        public static IEnumerable<TSource> LogProgress<TSource>(this ICollection<TSource> source, ILogger logger, int logAfterXItems = 500, string messageTemplate = "Job Progress...")
+        {
+            return LogProgress(source, logger, source.Count, logAfterXItems, messageTemplate);
+        }
+
+        public static IEnumerable<TSource> LogProgress<TSource>(this IEnumerable<TSource> source, ILogger logger, long totalNumberOfItems, int logAfterXItems = 500, string messageTemplate = "Job Progress...")
+        {
+            return source.Select((item, idx) =>
+            {
+                var progress = idx + 1;
+                LogProgress(logger, progress, totalNumberOfItems, logAfterXItems, messageTemplate);
+
+                return item;
+            });
+        }
+
+        public static void LogProgress(this ILogger logger, long currentProgress, long totalNumberOfItems, int logAfterXItems = 500, string messageTemplate = "Job Progress...")
+        {
+            var progress = currentProgress;
+            if (progress == 1 || progress % logAfterXItems == 0 || progress == totalNumberOfItems)
+            {
+                logger.ForContext("ProgressEntry", true).Debug($"{messageTemplate} {{Current}} of {{Total}} - {{Percentage:P1}}", progress, totalNumberOfItems, (progress / (double)totalNumberOfItems));
+            }
+        }
+    }
+}
