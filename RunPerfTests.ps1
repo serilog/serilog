1474e8a288d1bdddb47bf00de032a07e9bee9f66
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2019-09-07 12:49:11 -0700

    Copy performance test results

--- RunPerfTests.ps1
+++ RunPerfTests.ps1
@@ -1,14 +1,28 @@
 Push-Location $PSScriptRoot
 
+Remove-Item $PSScriptRoot/results/* -Recurse -Force
+
 ./Build.ps1
 
-foreach ($test in ls test/*.PerformanceTests) {
+foreach ($test in ls ./test/*.PerformanceTests) {
     Push-Location $test
 
-	echo "perf: Running performance test project in $test"
+	echo "perf: Running performance test project in $test";
 
     & dotnet test -c Release
     if($LASTEXITCODE -ne 0) { exit 2 }
+
+	echo "perf: Copying performance test results";
+	
+	foreach ($sdkResult in Get-ChildItem ./bin/Release/) {
+		$destinationPath = ($PSScriptRoot + "/results/" + $sdkResult.Name);
+		mkdir $destinationPath > $null;
+		foreach ($resultFile in Get-ChildItem ($sdkResult.FullName + "/") -Recurse -Filter *.md) {
+			$newFileName = $resultFile.Name.Replace($test.Name + ".", "").Replace("-report-github.md", ".md");
+			$fullDestination = ($destinationPath + "/" + $newFileName);
+			Copy-Item $resultFile.FullName -Destination $fullDestination;
+		}
+	}
 
     Pop-Location
 }
