1334a5bd19e9fffc02bdb45fff4226b170648747
Sergey Komisarchik <skomisarchik@usetech.ru>
2020-05-21 15:43:56 +0300

    avoid aliases for Build.ps1; set lf eol for all by default

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,4 @@
-echo "build: Build started"
+Write-Output "build: Build started"
 
 & dotnet --info
 & dotnet --list-sdks
@@ -6,25 +6,25 @@ echo "build: Build started"
 Push-Location $PSScriptRoot
 
 if(Test-Path .\artifacts) {
-    echo "build: Cleaning .\artifacts"
+    Write-Output "build: Cleaning .\artifacts"
     Remove-Item .\artifacts -Force -Recurse
 }
 
 & dotnet restore --no-cache
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:APPVEYOR_REPO_BRANCH];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:APPVEYOR_BUILD_NUMBER];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 $commitHash = $(git rev-parse --short HEAD)
 $buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
 
-echo "build: Package version suffix is $suffix"
-echo "build: Build version suffix is $buildSuffix" 
+Write-Output "build: Package version suffix is $suffix"
+Write-Output "build: Build version suffix is $buildSuffix"
 
-foreach ($src in dir src/*) {
+foreach ($src in Get-ChildItem src/*) {
     Push-Location $src
 
-    echo "build: Packaging project in $src"
+    Write-Output "build: Packaging project in $src"
 
     & dotnet build -c Release --version-suffix=$buildSuffix /p:ContinuousIntegrationBuild=true
 
@@ -38,10 +38,10 @@ foreach ($src in dir src/*) {
     Pop-Location
 }
 
-foreach ($test in dir test/*.Tests) {
+foreach ($test in Get-ChildItem test/*.Tests) {
     Push-Location $test
 
-    echo "build: Testing project in $test"
+    Write-Output "build: Testing project in $test"
 
     & dotnet test -c Release
     if($LASTEXITCODE -ne 0) { exit 3 }
@@ -49,10 +49,10 @@ foreach ($test in dir test/*.Tests) {
     Pop-Location
 }
 
-foreach ($test in dir test/*.PerformanceTests) {
+foreach ($test in Get-ChildItem test/*.PerformanceTests) {
     Push-Location $test
 
-    echo "build: Building performance test project in $test"
+    Write-Output "build: Building performance test project in $test"
 
     & dotnet build -c Release
     if($LASTEXITCODE -ne 0) { exit 2 }
