7497752dbbc9669b98cb2910b5bee7320d08f3f5
Thomas Hauser <Thomas.Hauser@p-und-z.de>
2020-07-07 10:16:58 +0200

    Add unit tests for new extension methods for lazy evaluated property values.

--- test/Serilog.Tests/Core/LoggerExtensionsTests.cs
+++ test/Serilog.Tests/Core/LoggerExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using Serilog;
 using Serilog.Events;
 using Serilog.Tests.Support;
 using Xunit;
@@ -7,6 +8,10 @@ namespace Serilog.Tests.Core
 {
     public class LoggerExtensionsTests
     {
+        #region Methods
+
+        #region Public Methods
+
         [Theory]
         [InlineData(LogEventLevel.Debug, LogEventLevel.Debug)]
         [InlineData(LogEventLevel.Debug, LogEventLevel.Information)]
@@ -51,5 +56,177 @@ namespace Serilog.Tests.Core
 
             Assert.True(!sink.SingleEvent.Properties.ContainsKey(propKey));
         }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, true)]
+        [InlineData(LogEventLevel.Debug, true)]
+        [InlineData(LogEventLevel.Information, false)]
+        [InlineData(LogEventLevel.Warning, false)]
+        [InlineData(LogEventLevel.Error, false)]
+        [InlineData(LogEventLevel.Fatal, false)]
+        public void ShouldNotWriteLogEventsWhenMinLevelIsHigherThanDebug(LogEventLevel logMinLevel, bool shouldBeWritten)
+        {
+            var sink = new CollectingSink();
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logMinLevel)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Debug("This is a {test}", () => "xxx");
+
+            if (shouldBeWritten)
+                Assert.True(sink.SingleEvent.Properties.ContainsKey("test"));
+            else
+                Assert.Empty(sink.Events);
+        }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, true)]
+        [InlineData(LogEventLevel.Debug, true)]
+        [InlineData(LogEventLevel.Information, true)]
+        [InlineData(LogEventLevel.Warning, true)]
+        [InlineData(LogEventLevel.Error, true)]
+        [InlineData(LogEventLevel.Fatal, false)]
+        public void ShouldNotWriteLogEventsWhenMinLevelIsHigherThanError(LogEventLevel logMinLevel, bool shouldBeWritten)
+        {
+            var sink = new CollectingSink();
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logMinLevel)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Error("This is a {test}", () => "xxx");
+
+            if (shouldBeWritten)
+                Assert.True(sink.SingleEvent.Properties.ContainsKey("test"));
+            else
+                Assert.Empty(sink.Events);
+        }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, true)]
+        [InlineData(LogEventLevel.Debug, true)]
+        [InlineData(LogEventLevel.Information, true)]
+        [InlineData(LogEventLevel.Warning, false)]
+        [InlineData(LogEventLevel.Error, false)]
+        [InlineData(LogEventLevel.Fatal, false)]
+        public void ShouldNotWriteLogEventsWhenMinLevelIsHigherThanInformation(LogEventLevel logMinLevel, bool shouldBeWritten)
+        {
+            var sink = new CollectingSink();
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logMinLevel)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Information("This is a {test}", () => "xxx");
+
+            if (shouldBeWritten)
+                Assert.True(sink.SingleEvent.Properties.ContainsKey("test"));
+            else
+                Assert.Empty(sink.Events);
+        }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, LogEventLevel.Verbose, true)]
+        [InlineData(LogEventLevel.Verbose, LogEventLevel.Debug, true)]
+        [InlineData(LogEventLevel.Verbose, LogEventLevel.Information, true)]
+        [InlineData(LogEventLevel.Verbose, LogEventLevel.Warning, true)]
+        [InlineData(LogEventLevel.Verbose, LogEventLevel.Error, true)]
+        [InlineData(LogEventLevel.Verbose, LogEventLevel.Fatal, true)]
+        [InlineData(LogEventLevel.Debug, LogEventLevel.Verbose, false)]
+        [InlineData(LogEventLevel.Debug, LogEventLevel.Debug, true)]
+        [InlineData(LogEventLevel.Debug, LogEventLevel.Information, true)]
+        [InlineData(LogEventLevel.Debug, LogEventLevel.Warning, true)]
+        [InlineData(LogEventLevel.Debug, LogEventLevel.Error, true)]
+        [InlineData(LogEventLevel.Debug, LogEventLevel.Fatal, true)]
+        [InlineData(LogEventLevel.Information, LogEventLevel.Verbose, false)]
+        [InlineData(LogEventLevel.Information, LogEventLevel.Debug, false)]
+        [InlineData(LogEventLevel.Information, LogEventLevel.Information, true)]
+        [InlineData(LogEventLevel.Information, LogEventLevel.Warning, true)]
+        [InlineData(LogEventLevel.Information, LogEventLevel.Error, true)]
+        [InlineData(LogEventLevel.Information, LogEventLevel.Fatal, true)]
+        [InlineData(LogEventLevel.Warning, LogEventLevel.Verbose, false)]
+        [InlineData(LogEventLevel.Warning, LogEventLevel.Debug, false)]
+        [InlineData(LogEventLevel.Warning, LogEventLevel.Information, false)]
+        [InlineData(LogEventLevel.Warning, LogEventLevel.Warning, true)]
+        [InlineData(LogEventLevel.Warning, LogEventLevel.Error, true)]
+        [InlineData(LogEventLevel.Warning, LogEventLevel.Fatal, true)]
+        [InlineData(LogEventLevel.Error, LogEventLevel.Verbose, false)]
+        [InlineData(LogEventLevel.Error, LogEventLevel.Debug, false)]
+        [InlineData(LogEventLevel.Error, LogEventLevel.Information, false)]
+        [InlineData(LogEventLevel.Error, LogEventLevel.Warning, false)]
+        [InlineData(LogEventLevel.Error, LogEventLevel.Error, true)]
+        [InlineData(LogEventLevel.Error, LogEventLevel.Fatal, true)]
+        [InlineData(LogEventLevel.Fatal, LogEventLevel.Verbose, false)]
+        [InlineData(LogEventLevel.Fatal, LogEventLevel.Debug, false)]
+        [InlineData(LogEventLevel.Fatal, LogEventLevel.Information, false)]
+        [InlineData(LogEventLevel.Fatal, LogEventLevel.Warning, false)]
+        [InlineData(LogEventLevel.Fatal, LogEventLevel.Error, false)]
+        [InlineData(LogEventLevel.Fatal, LogEventLevel.Fatal, true)]
+        public void ShouldNotWriteLogEventsWhenMinLevelIsHigherThanProvidedLogLevel(LogEventLevel logMinLevel, LogEventLevel logLevelToWrite, bool shouldBeWritten)
+        {
+            var sink = new CollectingSink();
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logMinLevel)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Write(logLevelToWrite, "This is a {test}", () => "xxx");
+
+            if (shouldBeWritten)
+                Assert.True(sink.SingleEvent.Properties.ContainsKey("test"));
+            else
+                Assert.Empty(sink.Events);
+        }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, true)]
+        [InlineData(LogEventLevel.Debug, false)]
+        [InlineData(LogEventLevel.Information, false)]
+        [InlineData(LogEventLevel.Warning, false)]
+        [InlineData(LogEventLevel.Error, false)]
+        [InlineData(LogEventLevel.Fatal, false)]
+        public void ShouldNotWriteLogEventsWhenMinLevelIsHigherThanVerbose(LogEventLevel logMinLevel, bool shouldBeWritten)
+        {
+            var sink = new CollectingSink();
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logMinLevel)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Verbose("This is a {test}", () => "xxx");
+
+            if (shouldBeWritten)
+                Assert.True(sink.SingleEvent.Properties.ContainsKey("test"));
+            else
+                Assert.Empty(sink.Events);
+        }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, true)]
+        [InlineData(LogEventLevel.Debug, true)]
+        [InlineData(LogEventLevel.Information, true)]
+        [InlineData(LogEventLevel.Warning, true)]
+        [InlineData(LogEventLevel.Error, false)]
+        [InlineData(LogEventLevel.Fatal, false)]
+        public void ShouldNotWriteLogEventsWhenMinLevelIsHigherThanWarning(LogEventLevel logMinLevel, bool shouldBeWritten)
+        {
+            var sink = new CollectingSink();
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logMinLevel)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Warning("This is a {test}", () => "xxx");
+
+            if (shouldBeWritten)
+                Assert.True(sink.SingleEvent.Properties.ContainsKey("test"));
+            else
+                Assert.Empty(sink.Events);
+        }
+
+        #endregion Public Methods
+
+        #endregion Methods
     }
 }
