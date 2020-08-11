58eeb5ba81e659bec78e05e1a05ac2da6ceb5a48
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2020-08-10 23:24:37 -0700

    Rollback changes

--- src/Serilog/Events/EventProperty.cs
+++ src/Serilog/Events/EventProperty.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Serilog Contributors
+// Copyright 2019 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System;
-using System.Runtime.CompilerServices;
 
 namespace Serilog.Events
 {
