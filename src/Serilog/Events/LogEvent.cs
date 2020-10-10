323f1de82321ee77d780ff81fa6619109eb26c59
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2020-10-09 22:50:32 -0700

    Add Documentation

--- src/Serilog/Events/LogEvent.cs
+++ src/Serilog/Events/LogEvent.cs
@@ -70,7 +70,7 @@ namespace Serilog.Events
         /// <param name="exception">An exception associated with the event, or null.</param>
         /// <param name="messageTemplate">The message template describing the event.</param>
         /// <param name="properties">Properties associated with the event, including those presented in <paramref name="messageTemplate"/>.</param>
-        /// <param name="numOfPropertiesViaEnrichers"></param>
+        /// <param name="numOfPropertiesViaEnrichers">The possible numbers of extra properties will be added via enrichers.</param>
         /// <exception cref="ArgumentNullException">When <paramref name="messageTemplate"/> is <code>null</code></exception>
         /// <exception cref="ArgumentNullException">When <paramref name="properties"/> is <code>null</code></exception>
         internal LogEvent(DateTimeOffset timestamp, LogEventLevel level, Exception exception, MessageTemplate messageTemplate, in EventProperty[] properties, int numOfPropertiesViaEnrichers = 0)
