753a630be2b3a8162ed6884fb198429add907ac1
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2020-12-23 23:52:22 -0800

    Improve Script

--- RunPerfTests.ps1
+++ RunPerfTests.ps1
@@ -1,3 +1,7 @@
+Param (
+    [string] $BenchmarkFilter = "*"
+)
+
 Push-Location $PSScriptRoot
 
 $destinationPath = ($PSScriptRoot + "/results/");
@@ -20,7 +24,7 @@ foreach ($test in ls .\test\*.PerformanceTests) {
     if (Test-Path "./BenchmarkDotNet.Artifacts/") { Remove-Item "./BenchmarkDotNet.Artifacts/" -Recurse -Force }
 
 	echo "perf: Running performance test project in $test";
-    & dotnet run -c Release --framework netcoreapp3.1 -- --filter *
+    & dotnet run -c Release --framework net5.0 -- --filter $BenchmarkFilter
 
     if($LASTEXITCODE -ne 0) {
         Pop-Location
