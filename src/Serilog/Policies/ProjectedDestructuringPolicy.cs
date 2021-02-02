bd9a94b74ee321d419350846f84b9269474fb295
stan <stanislav@yarmonov.pro>
2021-02-02 18:28:53 +0200

    ProjectedDestructuringPolicy made public.

--- src/Serilog/Policies/ProjectedDestructuringPolicy.cs
+++ src/Serilog/Policies/ProjectedDestructuringPolicy.cs
@@ -1,4 +1,5 @@
-﻿// Copyright 2013-2015 Serilog Contributors
+
+// Copyright 2013-2015 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,17 +19,33 @@ using Serilog.Events;
 
 namespace Serilog.Policies
 {
-    class ProjectedDestructuringPolicy : IDestructuringPolicy
+    /// <summary>
+    /// When destructuring objects, projects instances of the specified type with
+    /// the provided function.
+    /// </summary>
+    public class ProjectedDestructuringPolicy : IDestructuringPolicy
     {
         readonly Func<Type, bool> _canApply;
         readonly Func<object, object> _projection;
 
+        /// <summary>
+        /// Construct policy
+        /// </summary>
+        /// <param name="canApply">
+        /// Predicate for object type to check if policy can be applied.
+        /// </param>
+        /// <param name="projection">
+        /// Projection function transforming incoming type into the projected one.
+        /// </param>
         public ProjectedDestructuringPolicy(Func<Type, bool> canApply, Func<object, object> projection)
         {
             _canApply = canApply ?? throw new ArgumentNullException(nameof(canApply));
             _projection = projection ?? throw new ArgumentNullException(nameof(projection));
         }
 
+        /// <summary>
+        /// Implements <see cref="IDestructuringPolicy.TryDestructure(object, ILogEventPropertyValueFactory, out LogEventPropertyValue)"/>.
+        /// </summary>
         public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue result)
         {
             if (value == null) throw new ArgumentNullException(nameof(value));
