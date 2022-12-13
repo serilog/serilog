62ad2847b874850b87691f23df484b8cb724f4cc
Ivan Maximov <sungam3r@yandex.ru>
2022-12-13 18:10:44 +0300

    merge

--- test/PoolRunner/LoggerExtensions.cs
+++ test/PoolRunner/LoggerExtensions.cs
@@ -1,11 +1,5 @@
 using Microsoft.Extensions.ObjectPool;
 using Serilog.Core;
-using Serilog.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PoolRunner;
 
--- test/PoolRunner/NullSink.cs
+++ test/PoolRunner/NullSink.cs
@@ -1,10 +1,5 @@
 using Serilog.Core;
 using Serilog.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PoolRunner;
 
