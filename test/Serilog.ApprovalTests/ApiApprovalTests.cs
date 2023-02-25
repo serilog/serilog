d30786199e665d7515af4b96873fa10c575e3932
Simon Cropp <simon.cropp@gmail.com>
2023-02-25 22:30:56 +1100

    PublicApi_Should_Not_Change_Unintentionally

--- test/Serilog.ApprovalTests/ApiApprovalTests.cs
+++ test/Serilog.ApprovalTests/ApiApprovalTests.cs
@@ -10,16 +10,17 @@ public class ApiApprovalTests
 {
     /// <summary> Check for changes to the public APIs. </summary>
     /// <param name="type"> The type used as a marker for the assembly whose public API change you want to check. </param>
-    [Theory]
-    [InlineData(typeof(ILogger))]
-    public void PublicApi_Should_Not_Change_Unintentionally(Type type)
+    [Fact]
+    public void PublicApi_Should_Not_Change_Unintentionally()
     {
-        string publicApi = type.Assembly.GeneratePublicApi(new ApiGeneratorOptions
-        {
-            IncludeAssemblyAttributes = false,
-            ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" },
-        });
+        var assembly = typeof(ILogger).Assembly;
+        var publicApi = assembly.GeneratePublicApi(
+            new()
+            {
+                IncludeAssemblyAttributes = false,
+                ExcludeAttributes = new[] {"System.Diagnostics.DebuggerDisplayAttribute"},
+            });
 
-        publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((testMethodInfo, discriminator, fileType, fileExtension) => $"{type.Assembly.GetName().Name!}.{fileType}.{fileExtension}"));
+        publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((_, _, fileType, fileExtension) => $"{assembly.GetName().Name!}.{fileType}.{fileExtension}"));
     }
 }
