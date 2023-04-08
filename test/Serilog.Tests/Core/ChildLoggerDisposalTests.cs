1c565c7fb08ed4f7213643dfda8d4c6720c004da
Sergey Rogovtsev <srogovtsev@acumatica.com>
2023-04-08 10:10:52 -0400

    Closes serilog/serilog#1447: ability to dispose nested loggers

---
+++ test/Serilog.Tests/Core/ChildLoggerDisposalTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Serilog.Tests.Core;
+
+public class ChildLoggerDisposalTests
+{
+    [Fact]
+    public void ByDefaultChildLoggerIsNotDisposedOnOuterDisposal()
+    {
+        var child = new DisposableLogger();
+        var outer = new LoggerConfiguration()
+            .WriteTo.Logger(child)
+            .CreateLogger();
+        outer.Dispose();
+        Assert.False(child.IsDisposed);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void WhenExplicitlyConfiguredChildLoggerShouldBeDisposedAccordingToConfiguration(bool attemptDispose)
+    {
+        var child = new DisposableLogger();
+        var outer = new LoggerConfiguration()
+            .WriteTo.Logger(child, attemptDispose)
+            .CreateLogger();
+        outer.Dispose();
+        Assert.Equal(attemptDispose, child.IsDisposed);
+    }
+
+#if FEATURE_ASYNCDISPOSABLE
+    [Fact]
+    public async Task ByDefaultChildLoggerIsNotAsyncDisposedOnOuterDisposal()
+    {
+        var child = new DisposableLogger();
+        var outer = new LoggerConfiguration()
+            .WriteTo.Logger(child)
+            .CreateLogger();
+        await outer.DisposeAsync();
+        Assert.False(child.IsDisposedAsync);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task WhenExplicitlyConfiguredChildLoggerShouldBeAsyncDisposedAccordingToConfiguration(bool attemptDispose)
+    {
+        var child = new DisposableLogger();
+        var outer = new LoggerConfiguration()
+            .WriteTo.Logger(child, attemptDispose)
+            .CreateLogger();
+        await outer.DisposeAsync();
+        Assert.Equal(attemptDispose, child.IsDisposedAsync);
+    }
+#endif
+}
