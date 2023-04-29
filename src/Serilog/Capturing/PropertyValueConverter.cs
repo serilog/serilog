d13aeee50b581529eca7a272ee2bd913d649cbfd
Ivan Maximov <sungam3r@yandex.ru>
2023-04-29 22:54:16 +0300

    is

--- src/Serilog/Capturing/PropertyValueConverter.cs
+++ src/Serilog/Capturing/PropertyValueConverter.cs
@@ -346,7 +346,7 @@ partial class PropertyValueConverter : ILogEventPropertyFactory, ILogEventProper
 
     static bool TryGetDictionary(object value, Type valueType, [NotNullWhen(true)] out IDictionary? dictionary)
     {
-        if (valueType.IsConstructedGenericType)
+        if (value is IDictionary && valueType.IsConstructedGenericType)
         {
             var definition = valueType.GetGenericTypeDefinition();
             if ((definition == typeof(Dictionary<,>) || definition == typeof(System.Collections.ObjectModel.ReadOnlyDictionary<,>)) &&
