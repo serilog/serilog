4bd40f1241057ebeed2062811d4990236be48ba5
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-03-12 09:18:33 +1000

    Add LogEvent.UnstableAssembleFromParts(); fixes #2010 (#2018)

--- test/Serilog.Tests/Events/LogEventTests.cs
+++ test/Serilog.Tests/Events/LogEventTests.cs
@@ -14,4 +14,37 @@ public class LogEventTests
         Assert.Equal(traceId, copy.TraceId);
         Assert.Equal(spanId, copy.SpanId);
     }
+
+    [Fact]
+    public void UnstableAssembleFromPartsDoesNotAllocateDictionary()
+    {
+        var properties = new Dictionary<string, LogEventPropertyValue>();
+
+        var timestamp = Some.OffsetInstant();
+        var level = Warning;
+        var exception = Some.Exception();
+        var template = Some.MessageTemplate();
+        var traceId = ActivityTraceId.CreateRandom();
+        var spanId = ActivitySpanId.CreateRandom();
+
+        var evt = LogEvent.UnstableAssembleFromParts(
+            timestamp,
+            level,
+            exception,
+            template,
+            properties,
+            traceId,
+            spanId);
+
+        // Test is named according to this assertion because it's the reason for the existence of
+        // the UnstableAssembleFromParts() method.
+        Assert.Same(properties, evt.Properties);
+
+        Assert.Equal(timestamp, evt.Timestamp);
+        Assert.Equal(level, evt.Level);
+        Assert.Same(exception, evt.Exception);
+        Assert.Same(template, evt.MessageTemplate);
+        Assert.Equal(traceId, evt.TraceId!.Value);
+        Assert.Equal(spanId, evt.SpanId!.Value);
+    }
 }
