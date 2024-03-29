49cf142988bc850e0a898e580e9decf0b6491528
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2024-03-28 17:09:42 -0700

    Improve MessageTemplateParsingBenchmark

--- test/Serilog.PerformanceTests/MessageTemplateParsingBenchmark.cs
+++ test/Serilog.PerformanceTests/MessageTemplateParsingBenchmark.cs
@@ -7,7 +7,14 @@ namespace Serilog.PerformanceTests;
 public class MessageTemplateParsingBenchmark
 {
     MessageTemplateParser _parser = null!;
+
+    const string _SimpleTextTemplate = "Hello, world!";
+    const string _SinglePropertyTokenTemplate = "{Name}";
+    const string _SingleTextWithPropertyTemplate = "This is a new Log entry with some external {Data}";
+    const string _ManyPropertyTokenTemplate = "{Greeting}, {Name}!";
+    const string _MultipleTokensTemplate = "Hello, world! {Greeting}, {Name} - {{Escaped}} - {@Hello} {$World}";
     const string _DefaultConsoleOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
+    const string _BigTemplate = "Hello, world! {Greeting}, {Name} - {{Escaped}} - {@Hello} {$World} {Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception} Hello, world! {0} {-1} {2} {Test,-15} {Prop,50} {Qnt,5:000}";
 
     [GlobalSetup]
     public void Setup()
@@ -16,14 +23,50 @@ public class MessageTemplateParsingBenchmark
     }
 
     [Benchmark(Baseline = true)]
-    public void EmptyTemplate()
+    public object EmptyTemplate()
     {
-        _parser.Parse("");
+        return _parser.Parse("");
     }
 
     [Benchmark]
-    public void DefaultConsoleOutputTemplate()
+    public object SimpleTextTemplate()
     {
-        _parser.Parse(_DefaultConsoleOutputTemplate);
+        return _parser.Parse(_SimpleTextTemplate);
+    }
+
+    [Benchmark]
+    public object SinglePropertyTokenTemplate()
+    {
+        return _parser.Parse(_SinglePropertyTokenTemplate);
+    }
+
+    [Benchmark]
+    public object SingleTextWithPropertyTemplate()
+    {
+        return _parser.Parse(_SingleTextWithPropertyTemplate);
+    }
+
+    [Benchmark]
+    public object ManyPropertyTokenTemplate()
+    {
+        return _parser.Parse(_ManyPropertyTokenTemplate);
+    }
+
+    [Benchmark]
+    public object MultipleTokensTemplate()
+    {
+        return _parser.Parse(_MultipleTokensTemplate);
+    }
+
+    [Benchmark]
+    public object DefaultConsoleOutputTemplate()
+    {
+        return _parser.Parse(_DefaultConsoleOutputTemplate);
+    }
+
+    [Benchmark]
+    public object BigTemplate()
+    {
+        return _parser.Parse(_BigTemplate);
     }
 }
