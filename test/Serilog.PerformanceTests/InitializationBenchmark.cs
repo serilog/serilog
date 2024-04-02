a9d770b369129d65123b854985c31d0abc284440
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2024-04-01 23:04:55 -0700

    Update InitializationBenchmark

--- test/Serilog.PerformanceTests/InitializationBenchmark.cs
+++ test/Serilog.PerformanceTests/InitializationBenchmark.cs
@@ -5,63 +5,18 @@ public class InitializationBenchmark
 {
     static readonly LoggingLevelSwitch LoggingLevelSwitch = new(LogEventLevel.Verbose);
 
-    [Params(0, 1, 4)]
-    public int NumberOfSinks { get; set; }
-
-    [Params(0, 1, 4)]
-    public int NumberOfAuditSinks { get; set; }
-
-    [Params(0, 1, 4)]
-    public int NumberOfFilters { get; set; }
-
-    [Params(0, 1, 4)]
-    public int NumberOfProperties { get; set; }
-
-    [Benchmark(Baseline = true)]
-    public ILogger CreateBasicLogger()
-    {
-        return new LoggerConfiguration()
-            .AddManyProperties(NumberOfProperties)
-            .AddManyFilters(NumberOfFilters)
-            .AddManyAuditSink(NumberOfAuditSinks)
-            .AddManySink(NumberOfSinks)
-            .CreateLogger();
-    }
+    [Params(0, 1, 4, 100)]
+    public int NumberOfSinksFiltersAndProps { get; set; }
 
     [Benchmark]
-    public ILogger CreateLoggerWithLogContext()
-    {
-        return new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .AddManyProperties(NumberOfProperties)
-            .AddManyFilters(NumberOfFilters)
-            .AddManyAuditSink(NumberOfAuditSinks)
-            .AddManySink(NumberOfSinks)
-            .CreateLogger();
-    }
-
-    [Benchmark]
-    public ILogger CreateLoggerWithLoggingLevelSwitch()
+    public ILogger CreateLogger()
     {
         return new LoggerConfiguration()
             .MinimumLevel.ControlledBy(LoggingLevelSwitch)
-            .AddManyProperties(NumberOfProperties)
-            .AddManyFilters(NumberOfFilters)
-            .AddManyAuditSink(NumberOfAuditSinks)
-            .AddManySink(NumberOfSinks)
-            .CreateLogger();
-    }
-
-    [Benchmark]
-    public ILogger CreateLoggerComplex()
-    {
-        return new LoggerConfiguration()
-            .MinimumLevel.ControlledBy(LoggingLevelSwitch)
-            .Enrich.FromLogContext()
-            .AddManyProperties(NumberOfProperties)
-            .AddManyFilters(NumberOfFilters)
-            .AddManyAuditSink(NumberOfAuditSinks)
-            .AddManySink(NumberOfSinks)
+            .AddManyProperties(NumberOfSinksFiltersAndProps)
+            .AddManyFilters(NumberOfSinksFiltersAndProps)
+            .AddManyAuditSink(NumberOfSinksFiltersAndProps)
+            .AddManySink(NumberOfSinksFiltersAndProps)
             .CreateLogger();
     }
 }
