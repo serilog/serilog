e8de3480ce935f2efd4ccf9907a7f03465586168
Andy Gocke <andy@commentout.net>
2025-05-10 14:25:40 -0700

    Fix AOT compatibility (#2175)
    * Fix AOT compatibility
    
    It looks like the code has drifted a bit and disrupted the feature switch that
    provided AOT compatibility. I've fixed the feature switch, added the AotTestApp
    to the solution to ensure that it builds when the solution does, configured the
    AotTestApp to publish on build so that it runs the IL analysis engine, and added
    a Serilog.props file to the NuGet package to enable the feature switch automatically
    whenever trimming is enabled.
    
    This should be much more likely to catch any regressions.
    
    * Fix comment

--- src/Serilog/Capturing/TrimConfiguration.cs
+++ src/Serilog/Capturing/TrimConfiguration.cs
@@ -4,11 +4,11 @@ namespace Serilog.Capturing;
 static class TrimConfiguration
 {
     /// <summary>
-    /// True if compiler-generated types are treated specially by Serilog during logging. The main example
+    /// True if structure-value types are treated specially by Serilog during logging. The main example
     /// of this would be anonymous types, which have a special compiler-generated form. If this switch is
     /// disabled, Serilog will not be able to destructure anonymous types, but will still be able to log
     /// them as scalar values.
     /// </summary>
-    public static bool IsCompilerGeneratedCodeSupported { get; } =
-        !AppContext.TryGetSwitch("Serilog.Capturing.IsCompilerGeneratedCodeSupported", out var isEnabled) || isEnabled;
+    public static bool IsStructureValueSupported { get; } =
+        !AppContext.TryGetSwitch("Serilog.Capturing.IsStructureValueSupported", out var isEnabled) || isEnabled;
 }
